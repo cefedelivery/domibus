2f4270d5ba50f2399bd9eadc6c06f97772a8ca42
idragusa <idragusa@gmail.com>
2016-04-19 09:25:15 +0200

    Add shebang line to setenv.sh

--- Domibus-MSH/src/main/assembly/setenv.sh
+++ Domibus-MSH/src/main/assembly/setenv.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #Please change CATALINA_HOME to the right folder
 #export CATALINA_HOME=<YOUR_INSTALLATION_PATH>
 #JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=4096m  -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
