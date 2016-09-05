c38cc5e090a71052a19b958d872c9ab29b30f19d
baciuco <cosmin.baciu@ext.ec.europa.eu>
2016-09-05 15:40:04 +0200

    [EDELIVERY-1142] - added unit tests

--- Domibus-MSH-jms-spi-helper/src/main/java/eu/domibus/jms/spi/helper/JMSSelectorUtilImpl.java
+++ Domibus-MSH-jms-spi-helper/src/main/java/eu/domibus/jms/spi/helper/JMSSelectorUtilImpl.java
@@ -59,9 +59,9 @@ public class JMSSelectorUtilImpl implements JMSSelectorUtil {
             selector.append(selector.length() > 0 ? " and " : "");
             selector.append(selectorClause);
         }
-        if (selector.length() == 0) {
-            selector.append("true");
-        }
+//        if (selector.length() == 0) {
+//            selector.append("true");
+//        }
         return selector.toString().trim();
     }
 }
