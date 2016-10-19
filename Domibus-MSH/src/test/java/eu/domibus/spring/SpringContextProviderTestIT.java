dd0aae868d4317d6fc0fa804d94a803381cdd95d
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2016-10-19 17:08:00 +0200

    [EDELIVERY-1540] - Fixed the SpringContextProviderTestIT

--- Domibus-MSH/src/test/java/eu/domibus/spring/SpringContextProviderTestIT.java
+++ Domibus-MSH/src/test/java/eu/domibus/spring/SpringContextProviderTestIT.java
@@ -1,10 +1,13 @@
 package eu.domibus.spring;
 
+import eu.domibus.api.xml.XMLUtil;
+import eu.domibus.xml.XMLUtilImpl;
 import mockit.Tested;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,12 +22,18 @@ public class SpringContextProviderTestIT {
 
     @Configuration
     static class ContextConfiguration {
+
+        @Bean
+        public SpringContextProvider xmlUtil() {
+            SpringContextProvider springContextProvider = new SpringContextProvider();
+            return springContextProvider;
+        }
     }
 
     @Test
     public void testGetApplicationContext() throws Exception {
         ApplicationContext applicationContext = SpringContextProvider.getApplicationContext();
-        Assert.assertNull(applicationContext);
+        Assert.assertNotNull(applicationContext);
 
     }
 }
