fa4f20f908eb735f11c2a569de0361e4c253f4ba
Ioana DRAGUSANU <idragusa@gmail.com>
2016-11-02 17:28:50 +0200

    [EDELIVERY-1325] Less memory

--- Domibus-MSH/src/main/assembly/setenv.sh
+++ Domibus-MSH/src/main/assembly/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Please change CATALINA_HOME to the right folder
 #export CATALINA_HOME=<YOUR_INSTALLATION_PATH>
-#JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx1024m -XX:MaxPermSize=1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
+#JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx128m -XX:MaxPermSize=128m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
