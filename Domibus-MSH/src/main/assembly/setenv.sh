6c6f8e09852ab1bcecd3b496238686ff280e0285
Ioana DRAGUSANU <idragusa@gmail.com>
2016-11-02 16:49:47 +0200

    [EDELIVERY-1325] Use local images in compose and reduce memory allocation in setenv.sh

--- Domibus-MSH/src/main/assembly/setenv.sh
+++ Domibus-MSH/src/main/assembly/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Please change CATALINA_HOME to the right folder
 #export CATALINA_HOME=<YOUR_INSTALLATION_PATH>
-#JAVA_OPTS="$JAVA_OPTS -Xms4096m -Xmx4096m -XX:MaxPermSize=4096m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
+#JAVA_OPTS="$JAVA_OPTS -Xms1024m -Xmx1024m -XX:MaxPermSize=1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
