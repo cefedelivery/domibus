640c16e017067546a8796caf6ec75e2bd3338138
venugar <arun-raj.venugopal@ext.ec.europa.eu>
2016-12-02 18:38:57 +0100

    [EDelivery-1555] Modifying Unit tests intermediate. Not to be patched to dev branch. To be continued.

--- Domibus-MSH/src/test/java/eu/domibus/ebms3/common/dao/CachingPModeProviderTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/ebms3/common/dao/CachingPModeProviderTest.java
@@ -2,8 +2,7 @@ package eu.domibus.ebms3.common.dao;
 
 import eu.domibus.api.xml.XMLUtil;
 import eu.domibus.common.dao.ConfigurationDAO;
-import eu.domibus.common.model.configuration.Configuration;
-import eu.domibus.common.model.configuration.Mpc;
+import eu.domibus.common.model.configuration.*;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
@@ -15,6 +14,9 @@ import org.springframework.jms.core.JmsOperations;
 
 import javax.persistence.EntityManager;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -31,7 +33,6 @@ public class CachingPModeProviderTest {
     private static final String DEFAULTMPC = "defaultMpc";
     private static final String ANOTHERMPC = "anotherMpc";
     private static final String NONEXISTANTMPC = "NonExistantMpc";
-
 
     @Injectable
     ConfigurationDAO configurationDAO;
@@ -55,14 +56,38 @@ public class CachingPModeProviderTest {
     CachingPModeProvider cachingPModeProvider;
 
     @Test
-    public void testIsMpcExistant() {
-        final Set<Mpc> testMpc = loadTestMpcs();
-        new Expectations() {{
-            cachingPModeProvider.getConfiguration().getMpcs();
-            result = testMpc;
-        }};
+    public void loadSamplePModeConfiguration() throws JAXBException {
+        InputStream xmlStream = getClass().getClassLoader().getResourceAsStream("SamplePModes/domibus-configuration-valid.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(Configuration.class);
 
-        Assert.assertEquals(Boolean.TRUE, cachingPModeProvider.isMpcExistant(DEFAULTMPC));
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        configuration = (Configuration) unmarshaller.unmarshal(xmlStream);
+
+
+        Mpcs mpcs = configuration.getMpcsXml();
+        System.out.println("mpcs:" + mpcs);
+        System.out.println("mpcs size:" + mpcs.getMpc().size());
+
+        configuration.setMpcs(new HashSet<>(configuration.getMpcsXml().getMpc()));
+        System.out.println(configuration.getMpcs().size());
+
+        configuration.getBusinessProcesses();
+    }
+
+    @Test
+    public void testIsMpcExistant() throws JAXBException {
+        if (configuration == null) {
+            loadSamplePModeConfiguration();
+        }
+        final Set<Mpc> testMpc = loadTestMpcs();
+/*        new Expectations() {{
+            cachingPModeProvider.getConfiguration().getMpcs();
+            result = configuration.getMpcs();
+        }};*/
+
+        Mpcs mpcs = configuration.getMpcsXml();
+        System.out.println(mpcs);
+//        Assert.assertEquals(Boolean.TRUE, cachingPModeProvider.isMpcExistant(DEFAULTMPC.toUpperCase()));
     }
 
     @Test
@@ -84,7 +109,7 @@ public class CachingPModeProviderTest {
             result = testMpc;
         }};
 
-        Assert.assertEquals(10, cachingPModeProvider.getRetentionDownloadedByMpcName(ANOTHERMPC));
+        Assert.assertEquals(10, cachingPModeProvider.getRetentionDownloadedByMpcName(ANOTHERMPC.toLowerCase()));
     }
 
     @Test
@@ -108,7 +133,7 @@ public class CachingPModeProviderTest {
             result = testMpc;
         }};
 
-        Assert.assertEquals(12400, cachingPModeProvider.getRetentionUndownloadedByMpcName(ANOTHERMPC));
+        Assert.assertEquals(12400, cachingPModeProvider.getRetentionUndownloadedByMpcName(ANOTHERMPC.toUpperCase()));
     }
 
     @Test
@@ -137,8 +162,9 @@ public class CachingPModeProviderTest {
         Assert.assertEquals(URI1, result.get(1));
     }
 
-    private Set<Mpc> loadTestMpcs() {
-        final Set<Mpc> testMpc = new HashSet();
+
+    protected Set<Mpc> loadTestMpcs() {
+        Set<Mpc> testMpc = new HashSet();
         Mpc mpc1 = new Mpc();
         mpc1.setName(DEFAULTMPC);
         mpc1.setQualifiedName(URI1);
@@ -159,4 +185,133 @@ public class CachingPModeProviderTest {
 
         return testMpc;
     }
+
+    protected BusinessProcesses loadTestBusinessProcesses() {
+        BusinessProcesses businessProcesses = new BusinessProcesses();
+        businessProcesses.setRoles(loadTestRoles());
+        businessProcesses.setPartyIdTypes(loadTestPartyIdTypes());
+        businessProcesses.setParties(loadTestParties(businessProcesses.getPartyIdTypes()));
+        businessProcesses.setMeps(loadTestMeps());
+        businessProcesses.setMepBindings(loadTestMepBindings());
+        return businessProcesses;
+    }
+
+    private Set<Role> loadTestRoles() {
+        Set<Role> testRole = new HashSet();
+
+        Role role1 = new Role();
+        role1.setName("defaultInitiatorRole");
+        role1.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator");
+        testRole.add(role1);
+
+        Role role2 = new Role();
+        role2.setName("defaultResponderRole");
+        role2.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder");
+        return testRole;
+    }
+
+    private Set<Party> loadTestParties(Set<PartyIdType> partyIdTypeSet) {
+        Set<Party> testPartySet = new HashSet<>();
+        for (PartyIdType partyIdType : partyIdTypeSet) {
+            if ("partyTypeUrn".equalsIgnoreCase(partyIdType.getName())) {
+                testPartySet.add(loadTestParty("red_gw", partyIdType));
+                testPartySet.add(loadTestParty("blue_gw", partyIdType));
+            }
+        }
+        return testPartySet;
+    }
+
+    private Set<PartyIdType> loadTestPartyIdTypes() {
+        Set<PartyIdType> testPartyIdTypeSet = new HashSet<>();
+
+        PartyIdType partyIdType1 = new PartyIdType();
+        partyIdType1.setName("partyTypeUrn");
+        partyIdType1.setValue("urn:oasis:names:tc:ebcore:partyid-type:unregistered");
+        testPartyIdTypeSet.add(partyIdType1);
+
+        PartyIdType partyIdType2 = new PartyIdType();
+        partyIdType2.setName("IncorrectPartyTypeUrn");
+        partyIdType2.setValue("IncorrectURN");
+        testPartyIdTypeSet.add(partyIdType2);
+
+        return testPartyIdTypeSet;
+    }
+
+    private Party loadTestParty(String partyName, PartyIdType partyIdType) {
+        Party party = new Party();
+
+        switch (partyName) {
+            case "red_gw": {
+                party.setName(partyName);
+                party.setEndpoint("http://localhost:9080/domibus/services/msh");
+
+                Identifier identifier = new Identifier();
+                identifier.setPartyId("domibus-red");
+                identifier.setPartyIdType(partyIdType);
+                party.getIdentifiers().add(identifier);
+                break;
+            }
+            case "blue_gw":
+            default: {
+                party.setName(partyName);
+                party.setEndpoint("http://localhost:8081/domibus/services/msh");
+
+                Identifier identifier = new Identifier();
+                identifier.setPartyId("domibus-blue");
+                identifier.setPartyIdType(partyIdType);
+                party.getIdentifiers().add(identifier);
+                break;
+            }
+        }
+        return party;
+    }
+
+    private Set<Mep> loadTestMeps() {
+        Set<Mep> testMepSet = new HashSet<>();
+        testMepSet.add(loadTestMep("oneway"));
+        testMepSet.add(loadTestMep("twoway"));
+        return testMepSet;
+    }
+
+    private Mep loadTestMep(String messageExchangePattern) {
+        Mep testMep = new Mep();
+        testMep.setName(messageExchangePattern);
+        switch (messageExchangePattern) {
+            case "oneway": {
+                testMep.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/oneWay");
+                break;
+            }
+            case "twoway":
+            default: {
+                testMep.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/twoWay");
+                break;
+            }
+        }
+        return testMep;
+    }
+
+    private Set<Binding> loadTestMepBindings() {
+        Set<Binding> testMepBindingSet = new HashSet<>();
+        testMepBindingSet.add(loadTestBinding("push"));
+        testMepBindingSet.add(loadTestBinding("pushAndPush"));
+        return testMepBindingSet;
+    }
+
+    private Binding loadTestBinding(String messageExchgPatternBinding) {
+        Binding testBinding = new Binding();
+        testBinding.setName(messageExchgPatternBinding);
+        switch (messageExchgPatternBinding) {
+            case "push": {
+                testBinding.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/push");
+                break;
+            }
+            case "pushAndPush":
+            default: {
+                testBinding.setValue("http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/push-and-push");
+                break;
+            }
+        }
+        return testBinding;
+    }
+
 }
