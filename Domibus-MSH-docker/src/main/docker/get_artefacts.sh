19460636a203212bcc76e53cd8e3c92a0068459c
Ioana Dragusanu <idragusa@gmail.com>
2016-12-20 23:56:26 +0200

    [EDELIVERY-1325] Address review changes

--- Domibus-MSH-docker/src/main/docker/get_artefacts.sh
+++ Domibus-MSH-docker/src/main/docker/get_artefacts.sh
@@ -16,18 +16,18 @@ cp $SOURCE_CODE/target/domibus-MSH-$DOMIBUS_VERSION-sample-configuration-and-tes
 cp -R $SOURCE_CODE/src/main/conf/domibus/policies/ ./tomcat/policies
 
 cd $SOURCE_CODE/../Domibus-MSH-docker/src/main/docker/mysql
-sudo docker build -t domibus/mysql:development .
+sudo docker build --build-arg DOMIBUS_VERSION=$DOMIBUS_VERSION --build-arg SQLSCRIPTS_VERSION=$SQLSCRIPTS_VERSION -t domibus/mysql:development .
 
 cd $SOURCE_CODE/../Domibus-MSH-docker/src/main/docker/tomcat
-sudo docker build --build-arg PARTY=blue -t domibus/tomcat/blue:development .
-sudo docker build --build-arg PARTY=red -t domibus/tomcat/red:development .
+sudo docker build --build-arg PARTY=blue --build-arg DOMIBUS_VERSION=$DOMIBUS_VERSION -t domibus/tomcat/blue:development .
+sudo docker build --build-arg PARTY=red --build-arg DOMIBUS_VERSION=$DOMIBUS_VERSION -t domibus/tomcat/red:development .
 
 cd $SOURCE_CODE/../Domibus-MSH-docker/src/main/docker/tomcat-mysql-c2-c3-compose
 sudo docker-compose up -d
 
 cd $SOURCE_CODE/
-while ! curl --output /dev/null --silent --head --fail http://localhost:8180/domibus/home; do sleep 1 && echo -n .; done;
-while ! curl --output /dev/null --silent --head --fail http://localhost:9080/domibus/home; do sleep 1 && echo -n .; done;
+i=0; until [ $i -eq 4 ] || [ curl --output /dev/null --silent --head --fail http://localhost:8180/domibus/home ]; do sleep $((i++)) && echo -n .; done;
+i=0; until [ $i -eq 4 ] || [ curl --output /dev/null --silent --head --fail http://localhost:9080/domibus/home ]; do sleep $((i++)) && echo -n .; done;
 mvn com.smartbear.soapui:soapui-pro-maven-plugin:5.1.2:test
 
 cd $SOURCE_CODE/../Domibus-MSH-soapui-tests
@@ -40,5 +40,14 @@ sed -i -e "s/localhost:8180/domibusred:8080/g" ./domibus-gw-sample-pmode-red.xml
 
 curl -X POST -F pmode=@./domibus-gw-sample-pmode-blue.xml http://localhost:8180/domibus/home/uploadPmodeFile
 curl -X POST -F pmode=@./domibus-gw-sample-pmode-red.xml http://localhost:9080/domibus/home/uploadPmodeFile
+
+export MYSQL_CONNECTOR="mysql-connector-java-5.1.40"
+
+sudo apt-get install -y wget
+sudo apt-get install -y unzip
+sudo wget https://dev.mysql.com/get/Downloads/Connector-J/$MYSQL_CONNECTOR.zip \
+    && sudo unzip -o $MYSQL_CONNECTOR.zip
+
+sudo cp ./$MYSQL_CONNECTOR-bin.jar ./src/main/soapui/lib
 
 mvn com.smartbear.soapui:soapui-pro-maven-plugin:5.1.2:test
