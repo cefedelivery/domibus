bc915c4706fa20afd3f9727f43cd1f67841c1b43
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-01-13 08:58:15 +0100

    [EDELIVERY-1628] - added java doc on the MDCKey

--- Domibus-MSH-logging/src/main/java/eu/domibus/logging/MDCKey.java
+++ Domibus-MSH-logging/src/main/java/eu/domibus/logging/MDCKey.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation used to mark a method that is putting one key or multiple in MDC
+ *
+ * @author Cosmin Baciu
+ * @since 3.3
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MDCKey {
