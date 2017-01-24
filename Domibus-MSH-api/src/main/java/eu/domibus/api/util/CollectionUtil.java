d07bc4dcd3d8c7e00609b8efb7353d8c26d13d6b
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-01-24 13:41:17 +0100

    [EDELIVERY-1493] - added javadoc in CollectionUtil

--- Domibus-MSH-api/src/main/java/eu/domibus/api/util/CollectionUtil.java
+++ Domibus-MSH-api/src/main/java/eu/domibus/api/util/CollectionUtil.java
@@ -8,5 +8,9 @@ import java.util.List;
  */
 public interface CollectionUtil {
 
+    /**
+     * Returns a view of the portion of this list between the specified <tt>fromIndex</tt>, inclusive, and <tt>toIndex</tt>, exclusive.
+     * In case the <tt>list</tt> is null or the condition <tt>fromIndex >= list.size() || toIndex <= 0 || fromIndex >= toIndex</tt> is not met it returns and empty list
+     */
     <T> List<T> safeSubList(List<T> list, int fromIndex, int toIndex);
 }
