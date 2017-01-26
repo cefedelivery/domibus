d18b34819bc624184e6a320638cedb37ea305471
venugar <arun-raj.venugopal@ext.ec.europa.eu>
2017-01-26 18:20:56 +0100

    [EDelivery-1542] Adding Downloaded message status

--- Domibus-MSH/src/test/java/eu/domibus/plugin/handler/DatabaseMessageHandlerTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/plugin/handler/DatabaseMessageHandlerTest.java
@@ -22,6 +22,8 @@ import eu.domibus.ebms3.common.model.*;
 import eu.domibus.ebms3.common.model.Property;
 import eu.domibus.ebms3.common.model.Service;
 import eu.domibus.ebms3.security.util.AuthUtils;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.messaging.DuplicateMessageException;
 import eu.domibus.messaging.MessageNotFoundException;
 import eu.domibus.messaging.MessagingProcessingException;
@@ -32,8 +34,6 @@ import mockit.Injectable;
 import mockit.Tested;
 import mockit.Verifications;
 import mockit.integration.junit4.JMockit;
-import eu.domibus.logging.DomibusLogger;
-import eu.domibus.logging.DomibusLoggerFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -861,15 +861,70 @@ public class DatabaseMessageHandlerTest {
         }};
 
         final Submission sub = dmh.downloadMessage(MESS_ID);
+        Assert.assertNotNull(sub);
+        Assert.assertEquals(MESS_ID, sub.getMessageId());
+        Assert.assertEquals(submission, sub);
 
         new Verifications() {{
             authUtils.hasUserOrAdminRole();
             userMessageLogDao.findByMessageId(MESS_ID, MSHRole.RECEIVING);
+            userMessageLogDao.setMessageAsDownloaded(anyString);
             pModeProvider.getRetentionDownloadedByMpcURI(userMessage.getMpc());
+            messagingDao.clearPayloadData(anyString);
             signalMessageDao.findSignalMessagesByRefMessageId(MESS_ID);
-            Assert.assertNotNull(sub);
-            Assert.assertEquals(MESS_ID, sub.getMessageId());
-            Assert.assertEquals(submission, sub);
+            userMessageLogDao.setMessageAsDeleted(anyString);
+            signalMessageLogDao.setMessageAsDeleted(anyString);
+        }};
+
+    }
+
+    @Test
+    public void testDownloadMessageOK_RetentionNonZero() throws Exception {
+
+        final UserMessage userMessage = createUserMessage();
+
+        final Submission submission = new Submission();
+        submission.setMessageId(MESS_ID);
+
+        new Expectations() {{
+            authUtils.isUnsecureLoginAllowed();
+            result = false;
+
+            authUtils.getOriginalUserFromSecurityContext(SecurityContextHolder.getContext());
+            result = "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4";
+
+            messagingDao.findUserMessageByMessageId(MESS_ID);
+            result = userMessage;
+
+            userMessageLogDao.findByMessageId(MESS_ID, MSHRole.RECEIVING);
+            result = new UserMessageLog();
+
+            pModeProvider.getRetentionDownloadedByMpcURI(userMessage.getMpc());
+            result = 5;
+
+            transformer.transformFromMessaging(userMessage);
+            result = submission;
+
+        }};
+
+        final Submission sub = dmh.downloadMessage(MESS_ID);
+        Assert.assertNotNull(sub);
+        Assert.assertEquals(MESS_ID, sub.getMessageId());
+        Assert.assertEquals(submission, sub);
+
+        new Verifications() {{
+            authUtils.hasUserOrAdminRole();
+            userMessageLogDao.findByMessageId(MESS_ID, MSHRole.RECEIVING);
+            userMessageLogDao.setMessageAsDownloaded(anyString);
+            pModeProvider.getRetentionDownloadedByMpcURI(userMessage.getMpc());
+            messagingDao.clearPayloadData(anyString);
+            times = 0;
+            signalMessageDao.findSignalMessagesByRefMessageId(MESS_ID);
+            times = 0;
+            userMessageLogDao.setMessageAsDeleted(anyString);
+            times = 0;
+            signalMessageLogDao.setMessageAsDeleted(anyString);
+            times = 0;
         }};
 
     }
