304b7d38d8c401a2ce8ba4686f468161f170d79e
martifp <Federico.MARTINI@ext.ec.europa.eu>
2017-02-06 20:19:28 +0100

    EDELIVERY-1796 updates API

--- Domibus-Ext-Services-Api/src/main/java/eu/domibus/ext/services/MessagesMonitorService.java
+++ Domibus-Ext-Services-Api/src/main/java/eu/domibus/ext/services/MessagesMonitorService.java
@@ -6,7 +6,7 @@ import eu.domibus.ext.services.exceptions.MessagesMonitorException;
 import java.util.List;
 
 /**
- * This interface exposes all the available operations related to the messages monitor service.
+ * This interface exposes all the available operations related to the Messages Monitor Service.
  *
  * @author Federico Martini
  * @since 3.3
@@ -16,16 +16,16 @@ public interface MessagesMonitorService {
     /**
      * Operation to retrieve all the messages that are currently in a SEND_FAILURE status in the access point.
      *
-     * @return List list of unique message ids.
+     * @return List - a list of unique message ids.
      * @throws MessagesMonitorException
      */
     public List<String> getFailedMessages() throws MessagesMonitorException;
 
     /**
-     * Operation to get the time, in msecs, that a message has been in a SEND_FAILURE status in the access point.
+     * Operation to retrieve the time that a message has been in a SEND_FAILURE status in the access point.
      *
      * @param messageId Unique id of the message
-     * @return long
+     * @return long - the passed time in ms
      * @throws MessagesMonitorException
      */
     public long getSendFailureMessageTime(String messageId) throws MessagesMonitorException;
@@ -35,7 +35,7 @@ public interface MessagesMonitorService {
      * The retry mechanism, as configured in the PMode, will apply.
      *
      * @param messageId Unique id of the message
-     * @return boolean
+     * @return boolean - true if the operation succeeded
      * @throws MessagesMonitorException
      */
     public boolean restoreFailedMessage(String messageId) throws MessagesMonitorException;
@@ -44,7 +44,7 @@ public interface MessagesMonitorService {
      * Operation to delete a message which is in SEND_FAILURE status in the access point.
      *
      * @param messageId Unique id of the message
-     * @return boolean
+     * @return boolean - true if the operation succeeded
      * @throws MessagesMonitorException
      */
     public boolean deleteFailedMessage(String messageId) throws MessagesMonitorException;
@@ -53,7 +53,7 @@ public interface MessagesMonitorService {
      * Operation to get the history of the delivery attempts for a certain message.
      *
      * @param messageId Unique id of the message
-     * @return List
+     * @return List - a list of AttemptDTO
      * @throws MessagesMonitorException
      */
     public List<AttemptDTO> getAttemptsHistory(String messageId) throws MessagesMonitorException;
