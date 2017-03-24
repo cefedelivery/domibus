1dbb5abaa4172233517c296ee735520e1b5f1848
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-03-24 15:57:20 +0100

    [EDELIVERY-2055] - converted setEnv.sh to unix format

--- Domibus-MSH/src/main/assembly/setenv.sh
+++ Domibus-MSH/src/main/assembly/setenv.sh
@@ -2,3 +2,4 @@
 #Please change CATALINA_HOME to the right folder
 #export CATALINA_HOME=<YOUR_INSTALLATION_PATH>
 #JAVA_OPTS="$JAVA_OPTS -Xms4096m -Xmx4096m -XX:MaxPermSize=4096m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
+
