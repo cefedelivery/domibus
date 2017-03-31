597ab856fc390f3b66861ab62338d9757dd13e6d
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-03-31 17:51:02 +0200

    [EDELIVERY-1940] - added junit dependency to the domibus-services-ext-delegate

--- Domibus-MSH-ext-services-delegate/src/test/java/eu/domibus/ext/delegate/converter/DomibusDomainDefaultConverterTest.java
+++ Domibus-MSH-ext-services-delegate/src/test/java/eu/domibus/ext/delegate/converter/DomibusDomainDefaultConverterTest.java
@@ -1,8 +1,14 @@
 package eu.domibus.ext.delegate.converter;
 
+import org.junit.Test;
+
 /**
  * @author  migueti, Cosmin Baciu
  * @since 3.3
  */
 public class DomibusDomainDefaultConverterTest {
+
+    @Test
+    public void testConvert() throws Exception {
+    }
 }
