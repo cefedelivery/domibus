bc3d27f5e3d4da10530cb5fa93583d654395597f
Mircea Alexandru MUSAT <Mircea-Alexandru.MUSAT@ext.ec.europa.eu>
2017-04-25 18:45:09 +0200

    +adding test

--- Domibus-MSH/src/test/java/eu/domibus/ebms3/common/dao/PModeDaoTestIT.java
+++ Domibus-MSH/src/test/java/eu/domibus/ebms3/common/dao/PModeDaoTestIT.java
@@ -3,18 +3,21 @@ package eu.domibus.ebms3.common.dao;
 import eu.domibus.api.xml.UnmarshallerResult;
 import eu.domibus.api.xml.XMLUtil;
 import eu.domibus.common.dao.ConfigurationDAO;
+import eu.domibus.common.dao.ConfigurationRawDAO;
 import eu.domibus.common.dao.PModeDao;
 import eu.domibus.common.model.configuration.Configuration;
+import eu.domibus.common.model.configuration.ConfigurationRaw;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.messaging.XmlProcessingException;
 import eu.domibus.xml.XMLUtilImpl;
 import org.apache.commons.io.IOUtils;
-import eu.domibus.logging.DomibusLogger;
-import eu.domibus.logging.DomibusLoggerFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -50,6 +53,11 @@ public class PModeDaoTestIT {
         @Bean
         public ConfigurationDAO configurationDAO() {
             return Mockito.mock(ConfigurationDAO.class);
+        }
+
+        @Bean
+        public ConfigurationRawDAO configurationRawDAO() {
+            return Mockito.mock(ConfigurationRawDAO.class);
         }
 
         @Bean
@@ -89,11 +97,15 @@ public class PModeDaoTestIT {
     ConfigurationDAO configurationDAO;
 
     @Autowired
+    ConfigurationRawDAO configurationRawDAO;
+
+    @Autowired
     JAXBContext jaxbContext;
 
     @Before
     public void resetMocks() {
         Mockito.reset(configurationDAO);
+        Mockito.reset(configurationRawDAO);
     }
 
 
@@ -116,6 +128,14 @@ public class PModeDaoTestIT {
         Configuration original = unmarshallerResult.getResult();
         assertEquals(saved.getMpcsXml().getMpc().size(), original.getMpcsXml().getMpc().size());
         assertEquals(saved.getBusinessProcesses(), original.getBusinessProcesses());
+
+        ArgumentCaptor<ConfigurationRaw> rawConfig = ArgumentCaptor.forClass(ConfigurationRaw.class);
+        Mockito.verify(configurationRawDAO).create(rawConfig.capture());
+
+        final ConfigurationRaw raw = rawConfig.getValue();
+        assertNotNull(raw.getConfigurationDate());
+        assertEquals(raw.getXml(), pModeBytes);
+
     }
 
     @Test
