b8a2f8e7baa9199fe3c209d484b4c34362eba2f4
migueti <tiago.miguel@ext.ec.europa.eu>
2017-05-05 13:14:59 +0200

    [EDELIVERY-1988] Message Filter changes

--- Domibus-MSH-angular/src/app/messagefilter/messagefilter.component.ts
+++ Domibus-MSH-angular/src/app/messagefilter/messagefilter.component.ts
@@ -14,7 +14,83 @@ import {AlertService} from "../alert/alert.service";
 
 export class MessageFilterComponent {
 
-  plugins: Array<String>;
+  editing = {};
+  rows = [];
+
+  enableCancel = false;
+  enableSave = false;
+  enableDelete = false;
+
+  enableMoveUp = false;
+  enableMoveDown = false;
+
+  constructor() {
+    this.rows.push({
+      "plugin": "a",
+      "from": "a",
+      "to": "a",
+      "action": "a",
+      "service": "a"
+    });
+    // this.fetch((data) => {
+    //   this.rows = data;
+    // });
+  }
+
+  // fetch(cb) {
+  //   const req = new XMLHttpRequest();
+  //   req.open('GET', `https://github.com/swimlane/ngx-datatable/tree/master/assets/data/company.json`);
+  //
+  //   req.onload = () => {
+  //     cb(JSON.parse(req.response));
+  //   };
+  //
+  //   req.send();
+  // }
+
+  updateValue(event, cell, cellValue, row) {
+    this.editing[row.$$index + '-' + cell] = false;
+    this.rows[row.$$index][cell] = event.target.value;
+  }
+
+  buttonNew() {
+    this.enableCancel = true;
+    this.enableSave = true;
+    this.enableDelete = false;
+    this.rows.push({
+      "plugin": "<>",
+      "from": "<>",
+      "to": "<>",
+      "action": "<>",
+      "service": "<>"});
+  }
+
+  buttonCancel() {
+    this.enableCancel = false;
+    this.enableSave = false;
+    this.enableDelete = false;
+  }
+
+  buttonSave() {
+    this.enableCancel = false;
+    this.enableSave = false;
+    this.enableDelete = false;
+  }
+
+  buttonDelete() {
+    this.enableDelete = false;
+  }
+
+  buttonMoveUp() {
+
+  }
+
+  buttonMoveDown() {
+
+  }
+
+
+  /*plugins: Array<String>;
 
   enableCancel = false;
   enableSave = false;
@@ -44,6 +120,6 @@ export class MessageFilterComponent {
 
   buttonDelete() {
     this.enableDelete = false;
-  }
+  }*/
 
 }
