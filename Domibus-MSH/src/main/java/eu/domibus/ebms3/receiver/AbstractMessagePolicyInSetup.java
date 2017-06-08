f709f14cf4a863d1d45dc25753be3b8f26bd1527
dussath <thomas.dussart@ext.ec.europa.eu>
2017-06-08 18:09:27 +0200

    [EDELIVERY-1859] Save user message and aknowledge

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/receiver/AbstractMessagePolicyInSetup.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/receiver/AbstractMessagePolicyInSetup.java
@@ -3,6 +3,7 @@ package eu.domibus.ebms3.receiver;
 import eu.domibus.common.model.configuration.LegConfiguration;
 import eu.domibus.ebms3.common.model.MessageInfo;
 import eu.domibus.ebms3.common.model.Messaging;
+import eu.domibus.ebms3.sender.MSHDispatcher;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.cxf.binding.soap.SoapMessage;
@@ -23,9 +24,12 @@ public abstract class AbstractMessagePolicyInSetup implements MessagePolicyInSet
     protected abstract String getMessageId();
 
 
-    protected void addMessageId() {
+    protected void setUpMessage(final String pmodeKey) {
         //set the messageId in the MDC context
         LOG.putMDC(DomibusLogger.MDC_MESSAGE_ID, getMessageId());
         message.getExchange().put(MessageInfo.MESSAGE_ID_CONTEXT_PROPERTY, getMessageId());
+        message.put(MSHDispatcher.PMODE_KEY_CONTEXT_PROPERTY, pmodeKey);
+        //FIXME: Test!!!!
+        message.getExchange().put(MSHDispatcher.PMODE_KEY_CONTEXT_PROPERTY, pmodeKey);
     }
 }
