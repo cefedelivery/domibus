1f37645b33ae7b32e241bbfe4aaea55d6c7a9f77
Mircea Alexandru MUSAT <Mircea-Alexandru.MUSAT@ext.ec.europa.eu>
2017-07-11 15:57:38 +0200

    +TC improvement

--- Domibus-MSH/src/test/java/eu/domibus/ebms3/common/validators/RolesValidatorTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/ebms3/common/validators/RolesValidatorTest.java
@@ -2,23 +2,23 @@ package eu.domibus.ebms3.common.validators;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import eu.domibus.common.model.configuration.*;
-import eu.domibus.common.model.configuration.Process;
+import eu.domibus.common.model.configuration.Configuration;
+import eu.domibus.common.model.configuration.Party;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.ReflectionUtils;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by musatmi on 10/07/2017.
@@ -53,7 +53,12 @@ public class RolesValidatorTest {
     }
 
     private Configuration newConfiguration() throws IOException {
-        return mapper.readValue("{\"businessProcesses\": {\"processes\": [{\"name\": \"TestProcess\",\"initiatorRole\": {\"name\": \"eCODEXRole\",\"value\": \"GW\"},\"responderRole\": {\"name\": \"eCODEXRole\",\"value\": \"GW\"}}]}}", Configuration.class);
+        return mapper.readValue(getResourceAsString("Configuration.json"), Configuration.class);
+    }
+
+    private String getResourceAsString(String resourceName) throws IOException {
+        ClassPathResource json = new ClassPathResource(this.getClass().getPackage().getName().replaceAll("\\.","\\/") + "/" + resourceName);
+        return IOUtils.toString(json.getInputStream());
     }
 
 }
