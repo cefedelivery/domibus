e481efe7ada05c00ac2aa426455bcf4cf3d48e4b
FERNANDES Henrique <hfernandes@linkare.com>
2017-07-19 14:16:30 +0100

    [EDELIVERY-2360] Adding FS Plugin Properties

--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSRetryService.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSRetryService.java
@@ -20,13 +20,13 @@ package eu.domibus.plugin.fs.worker;
 
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
+import eu.domibus.plugin.fs.FSPluginProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.Properties;
+import javax.annotation.Resource;
+
 
 /**
  * @author FERNANDES Henrique, GONCALVES Bruno
@@ -36,18 +36,11 @@ public class FSRetryService {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSRetryService.class);
 
-    @Autowired
-    @Qualifier("fsPluginProperties")
-    private Properties fsPluginProperties;
+    @Resource(name = "fsPluginProperties")
+    private FSPluginProperties fsPluginProperties;
 
     @Autowired
     private ApplicationContext appContext;
-
-    @PostConstruct
-    public void init() {
-        LOG.debug("Understanding plugin application context ({}) and classloader.", appContext);
-        LOG.debug("FS Plugin Properties: {}", fsPluginProperties);
-    }
 
     /**
      * Triggering the re-send means that the message file from the FAILED directory will be copied directly under the
@@ -55,6 +48,8 @@ public class FSRetryService {
      */
     public void resendFailedFSMessages() {
         LOG.debug("Resending failed file system messages...");
+        LOG.debug("location: {}", fsPluginProperties.getLocation());
+        LOG.debug("location DOMAIN1: {}", fsPluginProperties.getLocation("DOMAIN1"));
     }
 
 }
