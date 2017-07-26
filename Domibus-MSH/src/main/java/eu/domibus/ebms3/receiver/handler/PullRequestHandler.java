1924d282b3099f807a60e67446c89b7667e4910b
Tiago Miguel <tiago.miguel@ext.ec.europa.eu>
2017-07-26 09:55:40 +0200

    Rollback changes (Fix SONAR 25/07/2017) and included NOSONAR

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/receiver/handler/PullRequestHandler.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/receiver/handler/PullRequestHandler.java
@@ -143,7 +143,7 @@ public class PullRequestHandler {
             } catch (EbMS3Exception e1) {
                 throw new WebServiceException(e1);
             }
-        } catch (Exception e) {
+        } catch (Throwable e) { // NOSONAR: This was done on purpose.
             attemptError = e.getMessage();
             attemptStatus = MessageAttemptStatus.ERROR;
             throw e;
