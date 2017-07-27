7b8d95c7155728bf835fbb365d4b8da96096028f
Bruno GONÇALVES <bnazare@linkare.com>
2017-07-27 18:08:57 +0100

    [EDELIVERY-2360] Replaced FSRetryWorker with FSPurgeSentWorker Removed injection of ApplicationContext

--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSPurgeSentService.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSPurgeSentService.java
@@ -3,8 +3,7 @@ package eu.domibus.plugin.fs.worker;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.plugin.fs.FSPluginProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,22 +13,19 @@ import javax.annotation.Resource;
  * @author FERNANDES Henrique, GONCALVES Bruno
  */
 @Service
-public class FSRetryService {
+public class FSPurgeSentService {
 
-    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSRetryService.class);
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSPurgeSentService.class);
 
     @Resource(name = "fsPluginProperties")
     private FSPluginProperties fsPluginProperties;
 
-    @Autowired
-    private ApplicationContext appContext;
-
     /**
-     * Triggering the re-send means that the message file from the FAILED directory will be copied directly under the
-     * corresponding OUT directory and eventually it will be treated like a normal file.
+     * Triggering the purge means that the message files from the SENT directory 
+     * older than X seconds will be removed
      */
     public void resendFailedFSMessages() {
-        LOG.debug("Resending failed file system messages...");
+        LOG.debug("Purging sent file system messages...");
         LOG.debug("location: {}", fsPluginProperties.getLocation());
         LOG.debug("location DOMAIN1: {}", fsPluginProperties.getLocation("DOMAIN1"));
     }
--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSPurgeSentWorker.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSPurgeSentWorker.java
@@ -9,21 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
- * Quartz based worker responsible for the periodical execution of the FSRetryService.
+ * Quartz based worker responsible for the periodical execution of the FSPurgeSentService.
  *
  * @author FERNANDES Henrique, GONCALVES Bruno
  */
-@DisallowConcurrentExecution // Only one FSRetryWorker runs at any time on the same node
-public class FSRetryWorker extends QuartzJobBean {
+@DisallowConcurrentExecution // Only one FSPurgeSentWorker runs at any time on the same node
+public class FSPurgeSentWorker extends QuartzJobBean {
 
-    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSRetryWorker.class);
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(FSPurgeSentWorker.class);
 
     @Autowired
-    private FSRetryService retryService;
+    private FSPurgeSentService purgeSentService;
 
     @Override
     protected void executeInternal(final JobExecutionContext context) throws JobExecutionException {
-        retryService.resendFailedFSMessages();
+        purgeSentService.resendFailedFSMessages();
     }
 
 }
