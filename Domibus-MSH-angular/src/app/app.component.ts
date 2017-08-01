8f591ccd47cac60267b94d0034e28a26ae8426c8
Mircea <Mircea-Alexandru.musat@ext.ec.europa.eu>
2017-08-01 11:14:05 +0200

    +[EDELIVERY-2420] - Making the ng-datatable reclaim space when the menu is collapsed

--- Domibus-MSH-angular/src/app/app.component.ts
+++ Domibus-MSH-angular/src/app/app.component.ts
@@ -59,5 +59,14 @@ export class AppComponent implements OnInit {
   toggleMenu() {
     this.fullMenu = !this.fullMenu
     this.menuClass = this.fullMenu ? "menu-expanded" : "menu-collapsed"
+    setTimeout(() => {
+      var evt = document.createEvent("HTMLEvents")
+      evt.initEvent('resize', true, false)
+      window.dispatchEvent(evt)
+    }, 500)
+    //ugly hack but otherwise the ng-datatable doesn't resize when collapsing the menu
+    //alternatively this can be tried (https://github.com/swimlane/ngx-datatable/issues/193) but one has to implement it on every page
+    //containing a ng-datatable and it only works after one clicks inside the table
   }
+
 }
