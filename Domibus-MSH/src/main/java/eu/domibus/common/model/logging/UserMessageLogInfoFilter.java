3c497ad0f1343785f75bd68d0b15701f035b2811
dussath <thomas.dussart@ext.ec.europa.eu>
2017-08-11 16:58:25 +0200

    EDELIVERY-2455: - remove fetch from join

--- Domibus-MSH/src/main/java/eu/domibus/common/model/logging/UserMessageLogInfoFilter.java
+++ Domibus-MSH/src/main/java/eu/domibus/common/model/logging/UserMessageLogInfoFilter.java
@@ -15,10 +15,10 @@ public class UserMessageLogInfoFilter extends MessageLogInfoFilter {
         String query = "select new eu.domibus.common.model.logging.MessageLogInfo(log.messageId,log.messageStatus,log.notificationStatus,log.mshRole , log.messageType, log.deleted,log.received,log.sendAttempts, log.sendAttemptsMax,log.nextAttempt,message.collaborationInfo.conversationId, partyFrom.value, partyTo.value, propsFrom.value, propsTo.value, info.refToMessageId) from UserMessageLog log, " +
                 "UserMessage message " +
                 "left join log.messageInfo info " +
-                "left join fetch message.messageProperties.property propsFrom " +
-                "left join fetch message.messageProperties.property propsTo " +
-                "left join fetch message.partyInfo.from.partyId partyFrom " +
-                "left join fetch message.partyInfo.to.partyId partyTo " +
+                "left join message.messageProperties.property propsFrom " +
+                "left join message.messageProperties.property propsTo " +
+                "left join message.partyInfo.from.partyId partyFrom " +
+                "left join message.partyInfo.to.partyId partyTo " +
                 "where message.messageInfo = info and propsFrom.name = 'originalSender'" +
                 "and propsTo.name = 'finalRecipient'";
 
