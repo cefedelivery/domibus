bf3a54e5a67016517dfd8ed6e1aa0b3d848077bf
FERNANDES Henrique <hfernandes@linkare.com>
2017-08-16 16:06:24 +0100

    [EDELIVERY-2364, EDELIVERY-2366] Reviewed comments

--- Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/worker/FSPurgeSentWorkerTest.java
+++ Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/worker/FSPurgeSentWorkerTest.java
@@ -1,21 +1,3 @@
-/*
- * Copyright 2015 e-CODEX Project
- * Licensed under the EUPL, Version 1.1 or – as soon they
- * will be approved by the European Commission - subsequent
- * versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the
- * Licence.
- * You may obtain a copy of the Licence at:
- * http://ec.europa.eu/idabc/eupl.html
- * Unless required by applicable law or agreed to in
- * writing, software distributed under the Licence is
- * distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.
- * See the Licence for the specific language governing
- * permissions and limitations under the Licence.
- */
-
 package eu.domibus.plugin.fs.worker;
 
 import mockit.Injectable;
@@ -27,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.quartz.JobExecutionContext;
 
 /**
- * @author FERNANDES Henrique (hfernandes@linkare.com)
+ * @author FERNANDES Henrique, GONCALVES Bruno
  */
 @RunWith(JMockit.class)
 public class FSPurgeSentWorkerTest {
--- Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/worker/FSSendMessagesWorkerTest.java
+++ Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/worker/FSSendMessagesWorkerTest.java
@@ -1,38 +1,15 @@
-/*
- * Copyright 2015 e-CODEX Project
- * Licensed under the EUPL, Version 1.1 or – as soon they
- * will be approved by the European Commission - subsequent
- * versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the
- * Licence.
- * You may obtain a copy of the Licence at:
- * http://ec.europa.eu/idabc/eupl.html
- * Unless required by applicable law or agreed to in
- * writing, software distributed under the Licence is
- * distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.
- * See the Licence for the specific language governing
- * permissions and limitations under the Licence.
- */
-
 package eu.domibus.plugin.fs.worker;
 
-import eu.domibus.plugin.fs.FSMessage;
 import mockit.Injectable;
 import mockit.Tested;
-import mockit.Verifications;
 import mockit.VerificationsInOrder;
 import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.quartz.JobExecutionContext;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.*;
 
 /**
- * @author FERNANDES Henrique (hfernandes@linkare.com)
+ * @author FERNANDES Henrique, GONCALVES Bruno
  */
 @RunWith(JMockit.class)
 public class FSSendMessagesWorkerTest {
