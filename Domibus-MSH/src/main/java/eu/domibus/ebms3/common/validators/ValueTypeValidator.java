228b3145913644cab81febf15944e3901891877d
Ioana DRAGUSANU <idragusa@gmail.com>
2017-08-22 17:46:27 +0300

    [EDELIVERY-1252] Address new review comments

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/common/validators/ValueTypeValidator.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/common/validators/ValueTypeValidator.java
@@ -1,12 +1,13 @@
 package eu.domibus.ebms3.common.validators;
 
 import eu.domibus.common.model.configuration.*;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -24,6 +25,8 @@ import java.util.List;
 @Order(2)
 public class ValueTypeValidator implements ConfigurationValidator {
 
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(ValueTypeValidator.class);
+
     @Override
     public List<String> validate(Configuration configuration) {
 
@@ -32,38 +35,66 @@ public class ValueTypeValidator implements ConfigurationValidator {
         final BusinessProcesses businessProcesses = configuration.getBusinessProcesses();
 
         for (Party party : businessProcesses.getParties()) {
-            for (Identifier identifier : party.getIdentifiers()) {
-                if (identifier.getPartyIdType() == null ||
-                        (identifier.getPartyIdType() != null && StringUtils.isEmpty(identifier.getPartyIdType().getValue()))) {
-                    try {
-                        URI.create(identifier.getPartyId());
-                    } catch (IllegalArgumentException exc) {
-                        issues.add("PartyIdType is empty and the partyId is not an URI for " + party.getName());
-                    }
-                }
-            }
+            issues.addAll(validateIdentifiers(party));
         }
 
         for (Service service : businessProcesses.getServices()) {
-            if (StringUtils.isEmpty(service.getServiceType())) {
-                try {
-                    URI.create(service.getValue());
-                } catch (IllegalArgumentException exc) {
-                    issues.add("Service type is empty and the service value is not an URI for " + service.getName());
-                }
-            }
+            issues.addAll(validateService(service));
         }
 
         for (Agreement agreement : businessProcesses.getAgreements()) {
-            if (StringUtils.isEmpty(agreement.getType())) {
+            issues.addAll(validateAgreement(agreement));
+        }
+
+        return Collections.unmodifiableList(issues);
+    }
+
+    protected List<String> validateIdentifiers(Party party) {
+        List<String> issues = new ArrayList<>();
+        if(party == null){
+            return issues;
+        }
+
+        for (Identifier identifier : party.getIdentifiers()) {
+            if (identifier.getPartyIdType() == null ||
+                    (identifier.getPartyIdType() != null && StringUtils.isEmpty(identifier.getPartyIdType().getValue()))) {
                 try {
-                        URI.create(agreement.getValue());
+                    URI.create(identifier.getPartyId());
                 } catch (IllegalArgumentException exc) {
-                    issues.add("Agreement type is empty and the agreement value is not an URI for " + agreement.getName());
+                    String msg = "PartyIdType is empty and the partyId is not an URI for " + party.getName();
+                    issues.add(msg);
+                    LOG.debug(msg);
                 }
             }
         }
+        return Collections.unmodifiableList(issues);
+    }
 
+    protected List<String> validateService(Service service) {
+        List<String> issues = new ArrayList<>();
+        if (StringUtils.isEmpty(service.getServiceType())) {
+            try {
+                URI.create(service.getValue());
+            } catch (IllegalArgumentException exc) {
+                String msg = "Service type is empty and the service value is not an URI for " + service.getName();
+                issues.add(msg);
+                LOG.debug(msg);
+            }
+        }
+        return Collections.unmodifiableList(issues);
+    }
+
+    protected List<String> validateAgreement(Agreement agreement) {
+        List<String> issues = new ArrayList<>();
+        if (StringUtils.isEmpty(agreement.getType())) {
+            try {
+                URI.create(agreement.getValue());
+            } catch (IllegalArgumentException exc) {
+                String msg = "Agreement type is empty and the agreement value is not an URI for " + agreement.getName();
+                issues.add(msg);
+                LOG.debug(msg);
+            }
+        }
         return Collections.unmodifiableList(issues);
     }
 }
