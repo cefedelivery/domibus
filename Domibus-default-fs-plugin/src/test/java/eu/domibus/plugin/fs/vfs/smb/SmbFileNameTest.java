18689e6db9e7465ce0a65d6121dde5240a119cd0
Bruno GONÇALVES <bnazare@linkare.com>
2017-08-25 16:20:04 +0100

    [EDELIVERY-2529] Improved code coverage

--- Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/vfs/smb/SmbFileNameTest.java
+++ Domibus-default-fs-plugin/src/test/java/eu/domibus/plugin/fs/vfs/smb/SmbFileNameTest.java
@@ -52,5 +52,37 @@ public class SmbFileNameTest {
         Assert.assertEquals("smb://example.org:12345/sharename/file1", result2);
         Assert.assertSame(result1, result2);
     }
+
+    @Test
+    public void testEquals_Equal() {
+        SmbFileName fileName2 = new SmbFileName("smb", "example.org", 12345, "user", "password", "domain", "sharename", "/file1", FileType.FILE);
+        
+        boolean result1 = fileName.equals(fileName2);
+        boolean result2 = fileName2.equals(fileName);
+        
+        Assert.assertTrue(result1);
+        Assert.assertTrue(result2);
+    }
+    
+    @Test
+    public void testEquals_NotEqual() {
+        SmbFileName fileName2 = new SmbFileName("smb", "example.org", 12345, "user", "password", "otherdomain", "sharename", "/file1", FileType.FILE);
+        
+        boolean result1 = fileName.equals(fileName2);
+        boolean result2 = fileName2.equals(fileName);
+        
+        Assert.assertFalse(result1);
+        Assert.assertFalse(result2);
+    }
+
+    @Test
+    public void testHashCode() {
+        SmbFileName fileName2 = new SmbFileName("smb", "example.org", 12345, "user", "password", "domain", "sharename", "/file1", FileType.FILE);
+        
+        int result1 = fileName.hashCode();
+        int result2 = fileName2.hashCode();
+        
+        Assert.assertEquals(result1, result2);
+    }
     
 }
