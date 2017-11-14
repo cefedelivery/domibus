f18b6d6bd0371cbbeb66b8c7127665d86adcb147
Ioana DRAGUSANU <idragusa@gmail.com>
2017-11-14 14:50:28 +0200

    CRLVerifierService unit tests

--- Domibus-MSH/src/test/java/eu/domibus/core/security/CRLVerifierServiceImplTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/security/CRLVerifierServiceImplTest.java
@@ -1,7 +1,5 @@
 package eu.domibus.core.security;
 
-import eu.domibus.api.security.AuthenticationException;
-import eu.domibus.api.security.ICRLVerifierService;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
 import eu.domibus.pki.CertificateServiceImpl;
@@ -10,6 +8,8 @@ import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.*;
+import java.math.BigInteger;
 import java.security.cert.X509Certificate;
 
 import static org.junit.Assert.assertNotNull;
@@ -25,19 +25,28 @@ public class CRLVerifierServiceImplTest {
 
     private CertificateServiceImpl certificateService = new CertificateServiceImpl();
 
-    private static final String RESOURCE_PATH = "src/test/resources/eu/domibus/ebms3/common/dao/DynamicDiscoveryPModeProviderTest/";
-    private static final String TEST_KEYSTORE = "testkeystore.jks";
-    private static final String ALIAS_CN_AVAILABLE = "cn_available";
-    private static final String TEST_KEYSTORE_PASSWORD = "1234";
+    private static final String RESOURCE_PATH = "src/test/resources/eu/domibus/core/security/";
+    private static final String TEST_KEYSTORE = "gateway_keystore_crl.jks";
+    private static final String TEST_KEYSTORE_INVALID = "gateway_keystore_crl_invalid.jks";
+    private static final String ALIAS = "ut";
+    private static final String ALIAS_INVALID = "edelivery";
+    private static final String TEST_KEYSTORE_PASSWORD = "test123";
 
     @Tested
     CRLVerifierServiceImpl securityCRLVerifierServiceImpl;
 
     @Test
-    public void verifyCertificateCRLsTest() {
-        X509Certificate certificate = certificateService.loadCertificateFromJKSFile(RESOURCE_PATH + TEST_KEYSTORE, ALIAS_CN_AVAILABLE, TEST_KEYSTORE_PASSWORD);
+    public void verifyCertificateCRLsTest() throws IOException {
+        X509Certificate certificate = certificateService.loadCertificateFromJKSFile(RESOURCE_PATH + TEST_KEYSTORE, ALIAS, TEST_KEYSTORE_PASSWORD);
         assertNotNull(certificate);
         securityCRLVerifierServiceImpl.verifyCertificateCRLs(certificate);
     }
 
+    @Test
+    public void verifyCertificateCRLsSerialTest() throws IOException {
+        X509Certificate certificate = certificateService.loadCertificateFromJKSFile(RESOURCE_PATH + TEST_KEYSTORE_INVALID, ALIAS_INVALID, TEST_KEYSTORE_PASSWORD);
+        assertNotNull(certificate);
+        String serial = certificate.getSerialNumber().toString();
+        securityCRLVerifierServiceImpl.verifyCertificateCRLs(serial, "test.crl");
+    }
 }
