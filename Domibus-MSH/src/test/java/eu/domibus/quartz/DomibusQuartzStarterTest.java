913e4bc711eb9825763b15617cbb8a840ddf8564
Catalin-Emanuel ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2018-01-08 14:30:59 +0100

    [EDELIVERY-3010] Begin the Junit class

---
+++ Domibus-MSH/src/test/java/eu/domibus/quartz/DomibusQuartzStarterTest.java
@@ -0,0 +1,19 @@
+package eu.domibus.quartz;
+
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Catalin Enache
+ * @version 1.0
+ * @since 08/01/2018
+ */
+@RunWith(JMockit.class)
+public class DomibusQuartzStarterTest {
+
+    @Test
+    public void checkJobsAndStartScheduler() throws Exception {
+    }
+
+}
