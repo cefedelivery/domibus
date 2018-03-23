8c75aa640b813e3b1d16a0cab3b93da96e6963ec
Tiago Miguel <tiago.miguel@ext.ec.europa.eu>
2018-03-23 14:40:49 +0100

    EDELIVERY-3246: Test Service GUI

---
+++ Domibus-MSH-angular/src/app/testservice/testservice.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  templateUrl: 'testservice.component.html',
+  providers: []
+})
+
+export class TestServiceComponent {
+
+}
