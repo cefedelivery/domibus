a2f2b5f35f5e041e18af3daa3695a597d3ef959a
Catalin Enache <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2018-04-09 13:41:46 +0200

    [EDELIVERY-3147] prepares script for Bamboo

--- Domibus-MSH-soapui-tests/doit.sh
+++ Domibus-MSH-soapui-tests/doit.sh
@@ -5,17 +5,16 @@
 #cd domibus/Domibus-MSH-soapui-tests
 #sleep 500
 
-export JDBC_DRIVERS=/c/Work/Devel/Java/Project/Bamboo/repo/jdbcdrivers
-export REPO=/c/Work/Devel/Java/Project/Bamboo/repo
-export DOMIBUS_VERSION=4.0-SNAPSHOT
-export ORACLE_REPO=/c/Work/Devel/Java/Project/Bamboo/repo/Oracle/OracleDatabase
-
-
+export JDBC_DRIVERS=${bamboo.JDBC_DRIVERS}
+export REPO=${bamboo.REPO}
+export DOMIBUS_VERSION=${bamboo.DOMIBUS_VERSION}
+export ORACLE_REPO=${bamboo.ORACLE_REPO}
 
 
 PMODE_FILE_C2=./src/main/soapui/domibus-gw-sample-pmode-blue.xml
 PMODE_FILE_C3=./src/main/soapui/domibus-gw-sample-pmode-red.xml
-DOMIBUS_DISTRIBUTION=/c/Work/Devel/Java/Project/Source/domibus/Domibus-MSH-distribution/target
+DOMIBUS_DISTRIBUTION=${bamboo_agentWorkingDirectory}/${bamboo.buildKey}/domibus/Domibus-MSH-distribution/target
+
 
 cp ${JDBC_DRIVERS}/* ./src/main/soapui/lib
 
@@ -26,8 +25,8 @@ echo; echo "Getting IP addresses of Containers"
 #DB_C3="`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' testtctc_mysqlc3_1`"
 #DOM_C3="`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' testtctc_tomcatc3_1`"
 
-#../../docker/runTests/prepareForTests.sh ${PMODE_FILE_C2} ${PMODE_FILE_C3} \
-#http://localhost:8082/domibus http://localhost:8083/domibus
+../../docker/runTests/prepareForTests.sh ${PMODE_FILE_C2} ${PMODE_FILE_C3} \
+http://localhost:8082/domibus http://localhost:8083/domibus
 
 ../../docker/runTests/runTests.sh \
  ${DOMIBUS_DISTRIBUTION} \
