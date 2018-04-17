3429eda60b207d00c6b579752847ccebc17d80df
baciuco <cosmin.baciu@ext.ec.europa.eu>
2018-04-17 17:50:14 +0200

    [EDELIVERY-964] - Support for certificate management with multiple domains

--- Domibus-MSH/src/test/java/eu/domibus/web/rest/AuthenticationResourceTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/web/rest/AuthenticationResourceTest.java
@@ -1,9 +1,11 @@
 package eu.domibus.web.rest;
 
+import eu.domibus.api.multitenancy.DomainContextProvider;
 import eu.domibus.api.multitenancy.DomainService;
 import eu.domibus.common.model.security.User;
 import eu.domibus.common.model.security.UserDetail;
 import eu.domibus.common.util.WarningUtil;
+import eu.domibus.core.multitenancy.dao.UserDomainDao;
 import eu.domibus.security.AuthenticationService;
 import eu.domibus.web.rest.ro.LoginRO;
 import mockit.*;
@@ -12,7 +14,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.scheduling.SchedulingTaskExecutor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,26 +26,47 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(JMockit.class)
 public class AuthenticationResourceTest {
-    @Tested AuthenticationResource authenticationResource;
-    @Injectable AuthenticationService authenticationService;
+
+    @Tested
+    AuthenticationResource authenticationResource;
+
+    @Injectable
+    AuthenticationService authenticationService;
+
+    @Injectable
+    SchedulingTaskExecutor taskExecutor;
+
+    @Injectable
+    UserDomainDao userDomainDao;
+
+    @Injectable
+    DomainContextProvider domainContextProvider;
+
     @Mocked
     Logger LOG;
+
+
     @Test
     public void testWarningWhenDefaultPasswordUsed(@Mocked WarningUtil warningUtil, @Mocked final LoggerFactory loggerFactory) throws Exception {
         User user = new User("user", "user");
         LoginRO loginRO = new LoginRO();
         loginRO.setUsername("user");
         loginRO.setPassword("user");
-        final UserDetail userDetail=new UserDetail(user,true);
-        new Expectations(){{
-            authenticationService.authenticate("user","user", DomainService.DEFAULT_DOMAIN.getCode()); result=userDetail;
+        final UserDetail userDetail = new UserDetail(user, true);
+        new Expectations(authenticationResource) {{
+            authenticationResource.getDomainForUser(loginRO.getUsername());
+            result = DomainService.DEFAULT_DOMAIN.getCode();
+
+            authenticationService.authenticate("user", "user", DomainService.DEFAULT_DOMAIN.getCode());
+            result = userDetail;
         }};
-        authenticationResource.authenticate(loginRO,new MockHttpServletResponse());
-        new Verifications(){{
+        authenticationResource.authenticate(loginRO, new MockHttpServletResponse());
+        new Verifications() {{
             String message;
             WarningUtil.warnOutput(message = withCapture());
-            assertEquals("user is using default password.",message);
-            LOG.warn(withAny(""));times=1;
+            assertEquals("user is using default password.", message);
+            LOG.warn(withAny(""));
+            times = 1;
         }};
     }
 
