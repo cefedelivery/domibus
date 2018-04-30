19f42ebe26c33b50a55985640e317b4f105a958b
Joze RIHTARSIC <joze.rihtarsic@ext.ec.europa.eu>
2018-04-30 09:06:01 +0200

    change MessageSender.onMessage propagation from REQUIRED to Propagation.REQUIRES_NEW to enforce new transaction timeout 5min

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/MessageSender.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/MessageSender.java
@@ -200,7 +200,7 @@ public class MessageSender implements MessageListener {
         }
     }
 
-    @Transactional(propagation = Propagation.REQUIRED, timeout = 300)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 300)
     @MDCKey(DomibusLogger.MDC_MESSAGE_ID)
     public void onMessage(final Message message) {
         LOG.debug("Processing message [{}]", message);
