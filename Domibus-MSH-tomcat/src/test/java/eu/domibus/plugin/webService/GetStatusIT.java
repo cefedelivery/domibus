43e88a3a5e4787db30a38e17747add48a42fced3
baciuco <cosmin.baciu@ext.ec.europa.eu>
2018-05-08 11:28:11 +0200

    [EDELIVERY-3306] - Refactor the integration tests

--- Domibus-MSH-tomcat/src/test/java/eu/domibus/plugin/webService/GetStatusIT.java
+++ Domibus-MSH-tomcat/src/test/java/eu/domibus/plugin/webService/GetStatusIT.java
@@ -1,25 +1,29 @@
 package eu.domibus.plugin.webService;
 
 import eu.domibus.AbstractIT;
-import eu.domibus.common.MSHRole;
 import eu.domibus.common.dao.UserMessageLogDao;
-import eu.domibus.common.model.logging.UserMessageLog;
+import eu.domibus.common.model.configuration.Configuration;
 import eu.domibus.common.services.MessagingService;
-import eu.domibus.ebms3.common.model.MessageType;
-import eu.domibus.ebms3.common.model.Messaging;
-import eu.domibus.ebms3.common.model.UserMessage;
+import eu.domibus.messaging.XmlProcessingException;
 import eu.domibus.plugin.webService.generated.BackendInterface;
 import eu.domibus.plugin.webService.generated.MessageStatus;
 import eu.domibus.plugin.webService.generated.StatusFault;
 import eu.domibus.plugin.webService.generated.StatusRequest;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.Provider;
 import java.io.IOException;
-import java.util.Date;
 
 public class GetStatusIT extends AbstractIT {
 
@@ -32,23 +36,25 @@ public class GetStatusIT extends AbstractIT {
     @Autowired
     UserMessageLogDao userMessageLogDao;
 
+    @Autowired
+    Provider<SOAPMessage> mshWebservice;
+
+    @Before
+    public void before() throws IOException, XmlProcessingException {
+        final byte[] pmodeBytes = IOUtils.toByteArray(new ClassPathResource("dataset/pmode/PModeTemplate.xml").getInputStream());
+        final Configuration pModeConfiguration = pModeProvider.getPModeConfiguration(pmodeBytes);
+        configurationDAO.updateConfiguration(pModeConfiguration);
+    }
+
     @Test
     @Transactional(propagation = Propagation.REQUIRED)
-    public void testGetStatusReceived() throws StatusFault, IOException {
-        final UserMessage userMessage = getUserMessageTemplate();
-        String messageId = "2809cef6-240f-4792-bec1-7cb300a34679@domibus.eu";
-        userMessage.getMessageInfo().setMessageId(messageId);
-        Messaging messaging = new Messaging();
-        messaging.setUserMessage(userMessage);
-        messagingService.storeMessage(messaging, MSHRole.RECEIVING);
+    public void testGetStatusReceived() throws StatusFault, IOException, SOAPException, SAXException, ParserConfigurationException {
+        String filename = "SOAPMessage2.xml";
+        String messageId = "43bb6883-77d2-4a41-bac4-52a485d50084@domibus.eu";
+        SOAPMessage soapMessage = createSOAPMessage(filename);
+        mshWebservice.invoke(soapMessage);
 
-        UserMessageLog userMessageLog = new UserMessageLog();
-        userMessageLog.setMessageStatus(eu.domibus.common.MessageStatus.RECEIVED);
-        userMessageLog.setMessageId(messageId);
-        userMessageLog.setMessageType(MessageType.USER_MESSAGE);
-        userMessageLog.setMshRole(MSHRole.RECEIVING);
-        userMessageLog.setReceived(new Date());
-        userMessageLogDao.create(userMessageLog);
+        waitUntilMessageIsReceived(messageId);
 
         StatusRequest messageStatusRequest = createMessageStatusRequest(messageId);
         MessageStatus response = backendWebService.getStatus(messageStatusRequest);
