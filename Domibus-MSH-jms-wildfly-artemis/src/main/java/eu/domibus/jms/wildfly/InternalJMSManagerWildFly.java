3be41ee5bdbc0fe09c77d8a7c4213c6edceba984
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2018-05-16 17:55:37 +0200

    [EDELIVERY-1433] finish integration with ActiveMQ Artemis

---
+++ Domibus-MSH-jms-wildfly-artemis/src/main/java/eu/domibus/jms/wildfly/InternalJMSManagerWildFly.java
@@ -0,0 +1,368 @@
+package eu.domibus.jms.wildfly;
+
+import eu.domibus.api.jms.JMSDestinationHelper;
+import eu.domibus.jms.spi.InternalJMSDestination;
+import eu.domibus.jms.spi.InternalJMSException;
+import eu.domibus.jms.spi.InternalJMSManager;
+import eu.domibus.jms.spi.InternalJmsMessage;
+import eu.domibus.jms.spi.helper.JMSSelectorUtil;
+import eu.domibus.jms.spi.helper.JmsMessageCreator;
+import eu.domibus.logging.DomibusLogger;
+import eu.domibus.logging.DomibusLoggerFactory;
+import org.apache.activemq.artemis.api.jms.management.JMSQueueControl;
+import org.apache.activemq.artemis.api.jms.management.JMSServerControl;
+import org.apache.activemq.artemis.api.jms.management.TopicControl;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.BrowserCallback;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+import javax.jms.Queue;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerInvocationHandler;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.*;
+
+/**
+ * @author Catalin Enache
+ * @since 4.0
+ */
+@Component
+public class InternalJMSManagerWildFly implements InternalJMSManager {
+
+    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(InternalJMSManagerWildFly.class);
+
+    private static final String PROPERTY_OBJECT_NAME = "ObjectName";
+    private static final String PROPERTY_JNDI_NAME = "Jndi";
+
+    static final String MBEAN_PREFIX_QUEUE_TOPIC = "org.apache.activemq.artemis:type=Broker,brokerName=\"default\",module=JMS,serviceType=";
+
+    protected Map<String, ObjectName> queueMap;
+
+    protected Map<String, ObjectName> topicMap;
+
+    @Autowired
+    MBeanServer mBeanServer;
+
+    @Autowired
+    @Qualifier("jmsServerControl")
+    JMSServerControl jmsServerControl;
+
+    @Resource(name = "jmsSender")
+    private JmsOperations jmsOperations;
+
+    @Autowired
+    JMSDestinationHelper jmsDestinationHelper;
+
+    @Autowired
+    JMSSelectorUtil jmsSelectorUtil;
+
+    @Override
+    public Map<String, InternalJMSDestination> findDestinationsGroupedByFQName() {
+
+        Map<String, InternalJMSDestination> destinationMap = new TreeMap<>();
+
+        try {
+            Map<String, ObjectName> queueMap = getQueueMap();
+            for (ObjectName objectName : queueMap.values()) {
+                JMSQueueControl jmsQueueControl = MBeanServerInvocationHandler.newProxyInstance(mBeanServer, objectName, JMSQueueControl.class, false);
+                InternalJMSDestination internalJmsDestination = new InternalJMSDestination();
+                internalJmsDestination.setName(jmsQueueControl.getName());
+                internalJmsDestination.setType(InternalJMSDestination.QUEUE_TYPE);
+                internalJmsDestination.setNumberOfMessages(jmsQueueControl.getMessageCount());
+                internalJmsDestination.setProperty(PROPERTY_OBJECT_NAME, objectName);
+                internalJmsDestination.setProperty(PROPERTY_JNDI_NAME, jmsQueueControl.getAddress());
+                internalJmsDestination.setInternal(jmsDestinationHelper.isInternal(jmsQueueControl.getAddress()));
+                destinationMap.put(jmsQueueControl.getName(), internalJmsDestination);
+            }
+            return destinationMap;
+        } catch (Exception e) {
+            throw new InternalJMSException("Failed to build JMS destination map", e);
+        }
+    }
+
+    protected Map<String, ObjectName> getQueueMap() {
+        if (queueMap != null) {
+            return queueMap;
+        }
+
+        queueMap = new HashMap<>();
+        String[] queueNames = jmsServerControl.getQueueNames();
+
+        for (String queueName : queueNames) {
+            //TODO externalize this
+            String mbeanObjectName = MBEAN_PREFIX_QUEUE_TOPIC + "Queue,name=\"" + queueName + "\"";
+
+            try {
+                ObjectName objectName = ObjectName.getInstance(mbeanObjectName);
+                queueMap.put(queueName, objectName);
+            } catch (MalformedObjectNameException e) {
+                LOG.error("Error getting queue [" + queueName + "] using mbeanName [" + mbeanObjectName + "]", e);
+            }
+        }
+        return queueMap;
+    }
+
+    protected Map<String, ObjectName> getTopicMap() {
+        if (topicMap != null) {
+            return topicMap;
+        }
+
+        topicMap = new HashMap<>();
+        String[] topicNames = jmsServerControl.getTopicNames();
+        for (String topicName : topicNames) {
+            //TODO externalize this
+            String mbeanObjectName = MBEAN_PREFIX_QUEUE_TOPIC + "Topic,name=\"" + topicName + "\"";
+
+            try {
+                ObjectName objectName = ObjectName.getInstance(mbeanObjectName);
+                topicMap.put(topicName, objectName);
+            } catch (MalformedObjectNameException e) {
+                LOG.error("Error getting topic [" + topicName + "] using mbeanName [" + mbeanObjectName + "]", e);
+            }
+        }
+        return topicMap;
+    }
+
+    protected JMSQueueControl getQueueControl(ObjectName objectName) {
+        return MBeanServerInvocationHandler.newProxyInstance(mBeanServer, objectName, JMSQueueControl.class, false);
+    }
+
+    protected TopicControl getTopicControl(ObjectName objectName) {
+        return MBeanServerInvocationHandler.newProxyInstance(mBeanServer, objectName, TopicControl.class, false);
+    }
+
+    protected JMSQueueControl getQueueControl(String destName) {
+        ObjectName objectName = getQueueMap().get(destName);
+        if (objectName == null) {
+            throw new InternalJMSException("Queue [" + destName + "] does not exists");
+        }
+        return getQueueControl(objectName);
+    }
+
+    protected TopicControl getTopicControl(String destName) {
+        ObjectName objectName = getTopicMap().get(destName);
+        if (objectName == null) {
+            throw new InternalJMSException("Topic [" + destName + "] does not exists");
+        }
+        return getTopicControl(objectName);
+    }
+
+    protected Queue getQueue(String queueName) throws NamingException {
+        return lookupQueue(queueName);
+    }
+
+    protected Topic getTopic(String topicName) throws NamingException {
+        return lookupTopic(topicName);
+    }
+
+    protected String getJndiName(String destJndiName) {
+        return "java:/" + StringUtils.replace(destJndiName, ".", "/");
+    }
+
+    protected String getJndiName(InternalJMSDestination internalJmsDestination) {
+        String destinationJndi = internalJmsDestination.getProperty(PROPERTY_JNDI_NAME);
+        return "java:/" + StringUtils.replace(destinationJndi, ".", "/");
+    }
+
+    protected Queue lookupQueue(String destName) throws NamingException {
+        String destinationJndi = getJndiName(getQueueControl(destName).getAddress());
+        LOG.debug("Found JNDI [" + destinationJndi + "] for queue [" + destName + "]");
+        return InitialContext.doLookup(destinationJndi);
+    }
+
+    protected Topic lookupTopic(String destName) throws NamingException {
+        String destinationJndi = getJndiName(getTopicControl(destName).getAddress());
+        LOG.debug("Found JNDI [" + destinationJndi + "] for topic [" + destName + "]");
+        return InitialContext.doLookup(destinationJndi);
+    }
+
+    @Override
+    public void sendMessage(InternalJmsMessage message, String destName) {
+        try {
+            jmsOperations.send(lookupQueue(destName), new JmsMessageCreator(message));
+        } catch (NamingException e) {
+            throw new InternalJMSException("Error performing lookup for [" + destName + "]", e);
+        }
+    }
+
+    @Override
+    public void sendMessage(InternalJmsMessage message, Destination destination) {
+        jmsOperations.send(destination, new JmsMessageCreator(message));
+    }
+
+    @Override
+    public void deleteMessages(String source, String[] messageIds) {
+        JMSQueueControl queue = getQueueControl(source);
+        try {
+            queue.removeMessages(jmsSelectorUtil.getSelector(messageIds));
+        } catch (Exception e) {
+            throw new InternalJMSException("Failed to delete messages from source [" + source + "]:" + messageIds, e);
+        }
+    }
+
+    @Override
+    public InternalJmsMessage getMessage(String source, String messageId) {
+        String selector = jmsSelectorUtil.getSelector(messageId);
+
+        try {
+            List<InternalJmsMessage> messages = getMessagesFromDestination(source, selector);
+            if (!messages.isEmpty()) {
+                return messages.get(0);
+            }
+        } catch (Exception e) {
+            throw new InternalJMSException("Error getting messages for [" + source + "] with selector [" + selector + "]", e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<InternalJmsMessage> browseMessages(String source) {
+        return browseMessages(source, null, null, null, null);
+    }
+
+    @Override
+    public List<InternalJmsMessage> browseMessages(String source, String jmsType, Date fromDate, Date toDate, String selectorClause) {
+        if (StringUtils.isEmpty(source)) {
+            throw new InternalJMSException("Source has not been specified");
+        }
+        InternalJMSDestination destination = findDestinationsGroupedByFQName().get(source);
+        if (destination == null) {
+            throw new InternalJMSException("Could not find destination for [" + source + "]");
+        }
+        List<InternalJmsMessage> internalJmsMessages = new ArrayList<>();
+            String destinationType = destination.getType();
+            if ("Queue".equals(destinationType)) {
+                Map<String, Object> criteria = new HashMap<String, Object>();
+                if (jmsType != null) {
+                    criteria.put("JMSType", jmsType);
+                }
+                if (fromDate != null) {
+                    criteria.put("JMSTimestamp_from", fromDate.getTime());
+                }
+                if (toDate != null) {
+                    criteria.put("JMSTimestamp_to", toDate.getTime());
+                }
+                if (selectorClause != null) {
+                    criteria.put("selectorClause", selectorClause);
+                }
+                String selector = jmsSelectorUtil.getSelector(criteria);
+                try {
+                    internalJmsMessages.addAll(getMessagesFromDestination(source, selector));
+                } catch (Exception e) {
+                    throw new InternalJMSException("Error getting messages for [" + source + "] with selector [" + selector + "]", e);
+                }
+            } else {
+                throw new InternalJMSException("Unrecognized destination type [" + destinationType + "]");
+            }
+        return internalJmsMessages;
+    }
+
+    private List<InternalJmsMessage> getMessagesFromDestination(String destination, String selector) throws NamingException {
+        Queue queue = getQueue(destination);
+        return jmsOperations.browseSelected(queue, selector, new BrowserCallback<List<InternalJmsMessage>>() {
+            @Override
+            public List<InternalJmsMessage> doInJms(Session session, QueueBrowser browser) throws JMSException {
+                List<InternalJmsMessage> result = new ArrayList<>();
+                Enumeration enumeration = browser.getEnumeration();
+                while (enumeration.hasMoreElements()) {
+                    TextMessage textMessage = null;
+                    try {
+                        textMessage = (TextMessage) enumeration.nextElement();
+                        result.add(convert(textMessage));
+                    } catch (Exception e) {
+                        LOG.error("Error converting message [" + textMessage + "]", e);
+                    }
+
+                }
+                return result;
+            }
+        });
+    }
+
+    protected InternalJmsMessage convert(TextMessage textMessage) throws JMSException {
+        InternalJmsMessage result = new InternalJmsMessage();
+        result.setContent(textMessage.getText());
+        result.setId(textMessage.getJMSMessageID());
+        result.setTimestamp(new Date(textMessage.getJMSTimestamp()));
+        result.setType(textMessage.getJMSType());
+        Enumeration propertyNames = textMessage.getPropertyNames();
+
+        Map<String, Object> properties = new HashMap<>();
+        while (propertyNames.hasMoreElements()) {
+            String name = (String) propertyNames.nextElement();
+            Object objectProperty = textMessage.getObjectProperty(name);
+            properties.put(name, objectProperty);
+        }
+        result.setProperties(properties);
+        return result;
+    }
+
+    protected List<InternalJmsMessage> convert(Map<String, Object>[] maps) {
+        if (maps == null) {
+            return null;
+        }
+        List<InternalJmsMessage> result = new ArrayList<>();
+        for (Map<String, Object> map : maps) {
+            result.add(convert(map));
+        }
+        return result;
+    }
+
+    protected InternalJmsMessage convert(Map<String, Object> map) {
+        InternalJmsMessage result = new InternalJmsMessage();
+
+        result.setType((String) map.get("JMSType"));
+        Long jmsTimestamp = (Long) map.get("JMSTimestamp");
+        if (jmsTimestamp != null) {
+            result.setTimestamp(new Date(jmsTimestamp));
+        }
+
+        result.setId((String) map.get("JMSMessageID"));
+
+        Map<String, Object> properties = new HashMap<>();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            Object propertyValue = entry.getValue();
+            properties.put(entry.getKey(), propertyValue);
+        }
+        result.setProperties(properties);
+        return result;
+    }
+
+    @Override
+    public void moveMessages(String source, String destination, String[] messageIds) {
+        try {
+            JMSQueueControl queue = getQueueControl(source);
+            queue.moveMessages(jmsSelectorUtil.getSelector(messageIds), destination);
+        } catch (Exception e) {
+            throw new InternalJMSException("Failed to move messages from source [" + source + "] to destination [" + destination + "]:" + messageIds, e);
+        }
+    }
+
+    @Override
+    public InternalJmsMessage consumeMessage(String source, String customMessageId) {
+
+        InternalJmsMessage intJmsMsg = null;
+        String selector = "MESSAGE_ID='" + customMessageId + "' AND NOTIFICATION_TYPE ='MESSAGE_RECEIVED'";
+
+        try {
+            List<InternalJmsMessage> messages = getMessagesFromDestination(source, selector);
+            if (!messages.isEmpty()) {
+                intJmsMsg = messages.get(0);
+                // Deletes it
+                JMSQueueControl queue = getQueueControl(source);
+                queue.removeMessages(selector);
+            }
+        } catch (Exception ex) {
+            throw new InternalJMSException("Failed to consume message [" + customMessageId + "] from source [" + source + "]", ex);
+        }
+        return intJmsMsg;
+    }
+}
---
+++ Domibus-MSH-jms-wildfly-artemis/src/test/java/eu/domibus/jms/wildfly/SendJMSMessageOnWildFly.java
@@ -0,0 +1,115 @@
+package eu.domibus.jms.wildfly;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Hashtable;
+import java.util.UUID;
+
+public class SendJMSMessageOnWildFly {
+
+    private static final String PROVIDER_URL = "http-remoting://localhost:8888";
+    //the user has to have the necessary roles; the security configuration is done in the WildFly profile under  <security-settings>
+    private static final String USER = "jmssender";
+    private static final String PASSWORD = "jmssender";
+    private static final String CONNECTION_FACTORY_JNDI = "jms/RemoteConnectionFactory";
+    private static final String QUEUE = "jms/domibus.backend.jms.inQueue";
+
+    public static void main(String[] args) throws Exception {
+        new SendJMSMessageOnWildFly().run();
+    }
+
+    public void run() throws RuntimeException {
+        try {
+            InitialContext ic = getInitialContext(PROVIDER_URL, USER, PASSWORD);
+            Queue queue = (Queue) ic.lookup(QUEUE);
+            QueueConnectionFactory cf = (QueueConnectionFactory) ic.lookup(CONNECTION_FACTORY_JNDI);
+            QueueConnection qc = cf.createQueueConnection(USER, PASSWORD);
+            QueueSession queueSession = qc.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+            QueueSender queueSessionSender = queueSession.createSender(queue);
+            MapMessage mapMessage = createMapMessage(queueSession);
+            queueSessionSender.send(mapMessage);
+            ic.close();
+            System.out.println("Successfully sent message");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    InitialContext getInitialContext(String providerUrl, String userName, String password) throws Exception {
+        InitialContext ic = null;
+        if (providerUrl != null) {
+            Hashtable<String, String> env = new Hashtable<String, String>();
+            env.put(Context.PROVIDER_URL, providerUrl);
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+            if (userName != null) {
+                env.put(Context.SECURITY_PRINCIPAL, userName);
+            }
+            if (password != null) {
+                env.put(Context.SECURITY_CREDENTIALS, password);
+            }
+            ic = new InitialContext(env);
+        } else {
+            ic = new InitialContext();
+        }
+        return ic;
+    }
+
+    protected MapMessage createMapMessage(Session session) throws JMSException {
+        MapMessage messageMap = session.createMapMessage();
+
+        // Declare message as submit
+        messageMap.setStringProperty("messageType", "submitMessage");
+        messageMap.setStringProperty("messageId", UUID.randomUUID().toString());
+        // Uncomment to test refToMessageId that is too long, i.e. > 255
+        // messageMap.setStringProperty("refToMessageId", "0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-0079a47e-aesasa");
+
+        // Set up the Communication properties for the message
+        messageMap.setStringProperty("service", "bdx:noprocess");
+        //messageMap.setStringProperty("serviceType", "noSecurity");
+        //messageMap.setStringProperty("serviceType", "signOnly");
+        messageMap.setStringProperty("serviceType", "tc1");
+
+        messageMap.setStringProperty("action", "TC1Leg1");
+        messageMap.setStringProperty("conversationId", "123");
+        //messageMap.setStringProperty("fromPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:domibus-blue");
+        //messageMap.setStringProperty("fromPartyType", ""); // Mandatory but empty here because it is in the value of the party ID
+        messageMap.setStringProperty("fromPartyId", "domibus-blue");
+        messageMap.setStringProperty("fromPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered"); // Mandatory
+
+        messageMap.setStringProperty("fromRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator");
+
+        //messageMap.setStringProperty("toPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:domibus-red");
+        //messageMap.setStringProperty("toPartyType", ""); // Mandatory but empty here because it is in the value of the party ID
+        messageMap.setStringProperty("toPartyId", "domibus-red");
+        messageMap.setStringProperty("toPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered"); // Mandatory
+
+        messageMap.setStringProperty("toRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder");
+
+        messageMap.setStringProperty("originalSender", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
+        messageMap.setStringProperty("finalRecipient", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
+        messageMap.setStringProperty("protocol", "AS4");
+
+        messageMap.setJMSCorrelationID("12345");
+        //Set up the payload properties
+        messageMap.setStringProperty("totalNumberOfPayloads", "1");
+        messageMap.setStringProperty("payload_1_description", "message");
+        messageMap.setStringProperty("payload_1_mimeContentId", "cid:message");
+        messageMap.setStringProperty("payload_1_mimeType", "text/xml");
+
+        //messageMap.setStringProperty("p1InBody", "true"); // If true payload_1 will be sent in the body of the AS4 message. Only XML payloads may be sent in the AS4 message body. Optional
+
+        //send the payload in the JMS message as byte array
+        String pay1 = "<test>test</test>";
+        byte[] payload = pay1.getBytes();
+        messageMap.setBytes("payload_1", payload);
+
+
+        //send the payload as a file system reference
+//        messageMap.setStringProperty("payload_1_fileName", "1_2GB.zip");
+//        messageMap.setString("payload_1", "file:////C:/DEV/1_2GB.zip");
+
+        return messageMap;
+    }
+
+}
