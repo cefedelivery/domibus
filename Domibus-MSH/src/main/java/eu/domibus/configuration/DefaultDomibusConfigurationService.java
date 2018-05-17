7da283da18e7b55f627fe92b390a7e0230191694
dussath <thomas.dussart@ext.ce.europa.eu>
2018-05-17 23:45:49 +0200

    EDELIVERY-3357: - fix tests

--- Domibus-MSH/src/main/java/eu/domibus/configuration/DefaultDomibusConfigurationService.java
+++ Domibus-MSH/src/main/java/eu/domibus/configuration/DefaultDomibusConfigurationService.java
@@ -5,9 +5,8 @@ import eu.domibus.api.configuration.DomibusConfigurationService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 /**
  * @author Cosmin Baciu
@@ -24,38 +23,30 @@ public class DefaultDomibusConfigurationService implements DomibusConfigurationS
 
     private static final String H_2 = "h2";
 
-    private DataBaseEngine dataBaseEngine;
-
     @Autowired
     @Qualifier("domibusProperties")
     private java.util.Properties domibusProperties;
 
-    @PostConstruct
-    public void init() {
-        final String property = domibusProperties.getProperty(DATABASE_DIALECT);
-        if (property == null) {
-            throw new IllegalStateException("Database dialect not configured, please set property: domibus.entityManagerFactory.jpaProperty.hibernate.dialect");
-        }
-        if (StringUtils.containsIgnoreCase(property, MYSQL)) {
-            this.dataBaseEngine = DataBaseEngine.MYSQL;
-        } else if (StringUtils.containsIgnoreCase(property, ORACLE)) {
-            dataBaseEngine = DataBaseEngine.ORACLE;
-        } else if (StringUtils.containsIgnoreCase(property, H_2)) {
-            dataBaseEngine = DataBaseEngine.H2;
-        } else {
-            throw new IllegalStateException("Unsupporte database dialect:" + property);
-        }
-
-
-    }
     @Override
     public String getConfigLocation() {
         return System.getProperty(DOMIBUS_CONFIG_LOCATION);
     }
 
     @Override
+    @Cacheable
     public DataBaseEngine getDataBaseEngine() {
-        return dataBaseEngine;
-
+        final String property = domibusProperties.getProperty(DATABASE_DIALECT);
+        if (property == null) {
+            throw new IllegalStateException("Database dialect not configured, please set property: domibus.entityManagerFactory.jpaProperty.hibernate.dialect");
+        }
+        if (StringUtils.containsIgnoreCase(property, MYSQL)) {
+            return DataBaseEngine.MYSQL;
+        } else if (StringUtils.containsIgnoreCase(property, ORACLE)) {
+            return DataBaseEngine.ORACLE;
+        } else if (StringUtils.containsIgnoreCase(property, H_2)) {
+            return DataBaseEngine.H2;
+        } else {
+            throw new IllegalStateException("Unsupporte database dialect:" + property);
+        }
     }
 }
