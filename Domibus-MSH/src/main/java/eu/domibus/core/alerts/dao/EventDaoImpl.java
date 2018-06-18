1652dd7a0547f213a0c7e8089e315d79affbefa1
dussath <thomas.dussart@ext.ce.europa.eu>
2018-06-18 13:42:48 +0200

    EDELIVERY-3303: - Development start

--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/dao/EventDaoImpl.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/dao/EventDaoImpl.java
@@ -5,9 +5,8 @@ import eu.domibus.core.alerts.model.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EventDaoImpl<E extends Event> extends BasicDao<E> implements EventDao{
+public abstract class EventDaoImpl<E extends Event> extends BasicDao<E> implements EventDao{
 
-    private final static Logger LOG = LoggerFactory.getLogger(EventDaoImpl.class);
 
     /**
      * @param typeOfT The entity class this DAO provides access to
