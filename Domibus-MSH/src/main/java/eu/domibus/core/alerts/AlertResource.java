d21e09e8a240e5254ec1fda85a53e4759470a46e
dussath <thomas.dussart@ext.ce.europa.eu>
2018-07-03 15:19:58 +0200

    EDELIVERY-3303: - start rest impl

--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/AlertResource.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/AlertResource.java
@@ -1,18 +1,28 @@
 package eu.domibus.core.alerts;
 
+import com.google.common.collect.Lists;
 import eu.domibus.core.alerts.dao.AlertCriteria;
+import eu.domibus.core.alerts.model.common.AlertLevel;
+import eu.domibus.core.alerts.model.common.AlertType;
+import eu.domibus.core.alerts.model.common.MessageEvent;
 import eu.domibus.core.alerts.model.service.Alert;
 import eu.domibus.core.alerts.service.AlertService;
+import eu.domibus.ext.delegate.converter.DomainExtConverter;
+import eu.domibus.web.rest.ro.ErrorLogResultRO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/rest/alert")
+@RequestMapping(value = "/rest/alerts")
 public class AlertResource {
 
     private final static Logger LOG = LoggerFactory.getLogger(AlertResource.class);
@@ -20,7 +30,60 @@ public class AlertResource {
     @Autowired
     private AlertService alertService;
 
-    public List<Alert> findAlerts(AlertCriteria alertCriteria) {
-        return alertService.findAlerts(alertCriteria);
+    @Autowired
+    private DomainExtConverter domibusDomainConverter;
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<AlertRo> findAlerts(@RequestParam(value = "page", defaultValue = "1") int page,
+                                    @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                    @RequestParam(value = "size", defaultValue = "10") int size,
+                                    @RequestParam(value = "orderBy", required = false) String column,
+                                    @RequestParam(value = "processed", required = false) boolean processed,
+                                    @RequestParam(value = "alertType", required = false) String alertType,
+                                    @RequestParam(value = "alertId", required = false) String alertId,
+                                    @RequestParam(value = "alertLevel", required = false) String alertLevel,
+                                    @RequestParam(value = "creationFrom", required = false) String creationFrom,
+                                    @RequestParam(value = "creationTo", required = false) String creationTo,
+                                    @RequestParam(value = "reportingFrom", required = false) String reportingFrom,
+                                    @RequestParam(value = "reportingTo", required = false) String reportingTo,
+                                    @RequestParam(value = "parameters", required = false) String[] parameters
+                                    ) {
+        AlertCriteria alertCriteria=new AlertCriteria();
+        final List<AlertRo> convert = domibusDomainConverter.convert(alertService.findAlerts(alertCriteria), AlertRo.class);
+        return convert;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/types")
+    public List<String> getAlertTypes() {
+        final List<AlertType> alertTypes = Lists.newArrayList(AlertType.values());
+        return alertTypes.stream().map(Enum::name).collect(Collectors.toList());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/levels")
+    public List<String> getAlertLevels() {
+        final List<AlertLevel> alertLevels = Lists.newArrayList(AlertLevel.values());
+        return alertLevels.stream().map(Enum::name).collect(Collectors.toList());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/params")
+    public List<String> getAlertParameters(@RequestParam(value = "alertType") String aType) {
+        final AlertType alertType = AlertType.valueOf(aType);
+        switch (alertType) {
+            case MSG_COMMUNICATION_FAILURE:
+                final List<MessageEvent> messageEvents = Lists.newArrayList(MessageEvent.values());
+                return messageEvents.stream().map(Enum::name).collect(Collectors.toList());
+            case CERT_EXPIRED:
+                return null;
+            case CERT_IMMINENT_EXPIRATION:
+                return null;
+            case USER_ACCOUNT_DISABLED:
+                return null;
+            case USER_LOGIN_FAILURE:
+                return null;
+            default:
+                return null;
+        }
+
     }
 }
---
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/AlertRo.java
@@ -0,0 +1,123 @@
+package eu.domibus.core.alerts;
+
+import eu.domibus.core.alerts.model.common.AlertLevel;
+import eu.domibus.core.alerts.model.common.AlertStatus;
+import eu.domibus.core.alerts.model.common.AlertType;
+import eu.domibus.core.alerts.model.service.Event;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AlertRo {
+
+    private int entityId;
+
+    private boolean processed;
+
+    private Date processedTime;
+
+    private AlertType alertType;
+
+    private Date reportingTime;
+
+    private Integer attempts;
+
+    private Integer maxAttempts;
+
+    private Date reportingTimeFailure;
+
+    private AlertStatus alertStatus;
+
+    private AlertLevel alertLevel;
+
+    private Set<Event> events = new HashSet<>();
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+
+    public Date getProcessedTime() {
+        return processedTime;
+    }
+
+    public void setProcessedTime(Date processedTime) {
+        this.processedTime = processedTime;
+    }
+
+    public AlertType getAlertType() {
+        return alertType;
+    }
+
+    public void setAlertType(AlertType alertType) {
+        this.alertType = alertType;
+    }
+
+    public Date getReportingTime() {
+        return reportingTime;
+    }
+
+    public void setReportingTime(Date reportingTime) {
+        this.reportingTime = reportingTime;
+    }
+
+    public Integer getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(Integer attempts) {
+        this.attempts = attempts;
+    }
+
+    public Integer getMaxAttempts() {
+        return maxAttempts;
+    }
+
+    public void setMaxAttempts(Integer maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    public Date getReportingTimeFailure() {
+        return reportingTimeFailure;
+    }
+
+    public void setReportingTimeFailure(Date reportingTimeFailure) {
+        this.reportingTimeFailure = reportingTimeFailure;
+    }
+
+    public AlertStatus getAlertStatus() {
+        return alertStatus;
+    }
+
+    public void setAlertStatus(AlertStatus alertStatus) {
+        this.alertStatus = alertStatus;
+    }
+
+    public AlertLevel getAlertLevel() {
+        return alertLevel;
+    }
+
+    public void setAlertLevel(AlertLevel alertLevel) {
+        this.alertLevel = alertLevel;
+    }
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+}
