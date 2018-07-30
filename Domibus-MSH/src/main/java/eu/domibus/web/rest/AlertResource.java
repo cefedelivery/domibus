d7dfda7507b01fe099b81778b35d1ac5ef245487
Ion PERPEGEL <ion.perpegel@gmail.com>
2018-07-30 11:32:12 +0300

    EDELIVERY 3491: fix default value for page parameter

--- Domibus-MSH/src/main/java/eu/domibus/web/rest/AlertResource.java
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/AlertResource.java
@@ -42,7 +42,7 @@ public class AlertResource {
 
 
     @GetMapping
-    public AlertResult findAlerts(@RequestParam(value = "page", defaultValue = "1") int page,
+    public AlertResult findAlerts(@RequestParam(value = "page", defaultValue = "0") int page,
                                   @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
                                   @RequestParam(value = "asc", defaultValue = "true") Boolean ask,
                                   @RequestParam(value = "orderBy", required = false) String column,
@@ -133,7 +133,7 @@ public class AlertResource {
     }
 
     @GetMapping(path = "/csv")
-    public ResponseEntity<String> getCsv(@RequestParam(value = "page", defaultValue = "1") int page,
+    public ResponseEntity<String> getCsv(@RequestParam(value = "page", defaultValue = "0") int page,
                                          @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
                                          @RequestParam(value = "asc", defaultValue = "true") Boolean ask,
                                          @RequestParam(value = "orderBy", required = false) String column,
@@ -184,7 +184,7 @@ public class AlertResource {
 
     }
 
-    private AlertCriteria getAlertCriteria(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize, @RequestParam(value = "asc", defaultValue = "true") Boolean ask, @RequestParam(value = "orderBy", required = false) String column, @RequestParam(value = "processed", required = false) String processed, @RequestParam(value = "alertType", required = false) String alertType, @RequestParam(value = "alertId", required = false) Integer alertId, @RequestParam(value = "alertLevel", required = false) String alertLevel, @RequestParam(value = "creationFrom", required = false) String creationFrom, @RequestParam(value = "creationTo", required = false) String creationTo, @RequestParam(value = "reportingFrom", required = false) String reportingFrom, @RequestParam(value = "reportingTo", required = false) String reportingTo, @RequestParam(value = "parameters", required = false) String[] parameters) {
+    private AlertCriteria getAlertCriteria(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "pageSize", defaultValue = "10") int pageSize, @RequestParam(value = "asc", defaultValue = "true") Boolean ask, @RequestParam(value = "orderBy", required = false) String column, @RequestParam(value = "processed", required = false) String processed, @RequestParam(value = "alertType", required = false) String alertType, @RequestParam(value = "alertId", required = false) Integer alertId, @RequestParam(value = "alertLevel", required = false) String alertLevel, @RequestParam(value = "creationFrom", required = false) String creationFrom, @RequestParam(value = "creationTo", required = false) String creationTo, @RequestParam(value = "reportingFrom", required = false) String reportingFrom, @RequestParam(value = "reportingTo", required = false) String reportingTo, @RequestParam(value = "parameters", required = false) String[] parameters) {
         AlertCriteria alertCriteria = new AlertCriteria();
         alertCriteria.setPage(page);
         alertCriteria.setPageSize(pageSize);
