bd06f40ca8305323148e3374b4727b903463ce0b
Ioana DRAGUSANU <idragusa@gmail.com>
2018-08-17 11:24:52 +0300

    [EDELIVERY-3543] Address review comments or FS plugin error file

--- Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSProcessFileService.java
+++ Domibus-default-fs-plugin/src/main/java/eu/domibus/plugin/fs/worker/FSProcessFileService.java
@@ -38,7 +38,7 @@ public class FSProcessFileService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void processFile(FileObject processableFile) throws FileSystemException, JAXBException, MessagingProcessingException {
-        FileObject metadataFile = fsFilesManager.resolveSibling(processableFile, FSSendMessagesService.METADATA_FILE_NAME);
+        try(FileObject metadataFile = fsFilesManager.resolveSibling(processableFile, FSSendMessagesService.METADATA_FILE_NAME);) {
             if (metadataFile.exists()) {
                 UserMessage metadata = parseMetadata(metadataFile);
                 LOG.debug("Metadata found and valid: [{}]", processableFile.getName());
@@ -48,7 +48,7 @@ public class FSProcessFileService {
 
                 //we add mimetype later, base name and dataHandler now
                 fsPayloads.put(DEFAULT_CONTENT_ID, new FSPayload(null, processableFile.getName().getBaseName(), dataHandler));
-                FSMessage message= new FSMessage(fsPayloads, metadata);
+                FSMessage message = new FSMessage(fsPayloads, metadata);
                 String messageId = backendFSPlugin.submit(message);
                 LOG.info("Message submitted: [{}]", processableFile.getName());
 
@@ -56,6 +56,7 @@ public class FSProcessFileService {
             } else {
                 LOG.error("Metadata file is missing for " + processableFile.getName().getURI());
             }
+        }
     }
 
     private void renameProcessedFile(FileObject processableFile, String messageId) {
