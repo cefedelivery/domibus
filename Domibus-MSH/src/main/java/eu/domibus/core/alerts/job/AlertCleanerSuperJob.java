829d2c21b288e4f51eb0ac9d986010b68d9c248f
dussath <thomas.dussart@ext.ce.europa.eu>
2018-10-05 10:35:19 +0200

    EDELIVERY:3768 Create a special job parent class for super user.

--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/job/AlertCleanerSuperJob.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/job/AlertCleanerSuperJob.java
@@ -1,24 +1,23 @@
 package eu.domibus.core.alerts.job;
 
 import eu.domibus.core.alerts.service.AlertService;
+import eu.domibus.quartz.GeneralQuartzJobBean;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
  * @author Thomas Dussart
  * @since 4.0
  */
 @DisallowConcurrentExecution
-public class AlertCleanerSuperJob extends QuartzJobBean {
+public class AlertCleanerSuperJob extends GeneralQuartzJobBean {
 
     @Autowired
     private AlertService alertService;
 
-
     @Override
-    protected void executeInternal(JobExecutionContext context){
+    protected void executeJob(JobExecutionContext context) {
         alertService.cleanAlerts();
     }
 }
--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/job/AlertRetrySuperJob.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/job/AlertRetrySuperJob.java
@@ -1,23 +1,23 @@
 package eu.domibus.core.alerts.job;
 
 import eu.domibus.core.alerts.service.AlertService;
+import eu.domibus.quartz.GeneralQuartzJobBean;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
 /**
  * @author Thomas Dussart
  * @since 4.0
  */
 @DisallowConcurrentExecution //Only one SenderWorker runs at any time
-public class AlertRetrySuperJob extends QuartzJobBean {
+public class AlertRetrySuperJob extends GeneralQuartzJobBean {
 
     @Autowired
     private AlertService alertService;
 
     @Override
-    protected void executeInternal(JobExecutionContext context){
+    protected void executeJob(JobExecutionContext context){
         alertService.retrieveAndResendFailedAlerts();
     }
 }
