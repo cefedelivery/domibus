c1d4fe394b90c3011e3c894c6e5440ff585ff73c
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2018-10-12 17:23:36 +0200

    [EDELIVERY-1206] added GET method

---
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/ro/LoggingLevelRO.java
@@ -0,0 +1,30 @@
+package eu.domibus.web.rest.ro;
+
+/**
+ * Encapsulates the package/class name and the level of logging
+ *
+ * @author Catalin Enache
+ * @since 4.1
+ */
+public class LoggingLevelRO {
+
+    private String name;
+
+    private String level;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
---
+++ Domibus-MSH/src/main/java/eu/domibus/web/rest/ro/LoggingLevelResultRO.java
@@ -0,0 +1,14 @@
+package eu.domibus.web.rest.ro;
+
+public class LoggingLevelResultRO extends LoggingLevelRO {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
