aed372418942b30f18d2ef95edc12f3bfdf9eb3b
ion.perpegel <ion.perpegel@gmail.com>
2018-10-24 15:38:22 +0300

    [EDELIVERY-3964] remove logging

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/MSHDispatcher.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/MSHDispatcher.java
@@ -8,8 +8,6 @@ import eu.domibus.common.ErrorCode;
 import eu.domibus.common.MSHRole;
 import eu.domibus.common.exception.EbMS3Exception;
 import eu.domibus.common.model.configuration.LegConfiguration;
-import eu.domibus.logging.DomibusLogger;
-import eu.domibus.logging.DomibusLoggerFactory;
 import org.apache.neethi.Policy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,8 +25,6 @@ import java.net.ConnectException;
  */
 @Service
 public class MSHDispatcher {
-
-    private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(MSHDispatcher.class);
 
     public static final String MESSAGE_TYPE_IN = "MESSAGE_TYPE";
     public static final String MESSAGE_TYPE_OUT = "MESSAGE_TYPE_OUT";
@@ -60,8 +56,6 @@ public class MSHDispatcher {
             }
             EbMS3Exception ex = new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0005, "Error dispatching message to " + endpoint, null, exception);
             ex.setMshRole(MSHRole.SENDING);
-
-            LOG.error("Could not dispatch soap message", ex);
             throw ex;
         }
         return result;
