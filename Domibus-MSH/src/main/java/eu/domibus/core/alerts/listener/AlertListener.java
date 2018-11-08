08ccb5b46f307a101a1c202c7e4f52fb4187ea6f
ion.perpegel <ion.perpegel@gmail.com>
2018-11-08 14:52:43 +0200

    [EDELIVERY-3988] replicate 4.1 changes

--- Domibus-MSH/src/main/java/eu/domibus/core/alerts/listener/AlertListener.java
+++ Domibus-MSH/src/main/java/eu/domibus/core/alerts/listener/AlertListener.java
@@ -33,10 +33,10 @@ public class AlertListener {
             domainContextProvider.setCurrentDomain(domain);
             LOG.debug("Alert received:[{}] for domain:[{}]", alert, domain);
         } else {
+            domainContextProvider.clearCurrentDomain();
             LOG.debug("Super alert received:[{}]", alert);
         }
         alertDispatcherService.dispatch(alert);
     }
-
 
 }
