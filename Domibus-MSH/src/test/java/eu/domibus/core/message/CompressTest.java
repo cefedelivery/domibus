7de2ba435f206e82119bf8fe4ea87b7bb1614297
baciuco <cosmin.baciu@ext.ec.europa.eu>
2018-11-19 10:27:33 +0100

    [EDELIVERY-4085] - PoC for split and join files

--- Domibus-MSH/src/test/java/eu/domibus/core/message/CompressTest.java
+++ Domibus-MSH/src/test/java/eu/domibus/core/message/CompressTest.java
@@ -2,13 +2,9 @@ package eu.domibus.core.message;
 
 import mockit.integration.junit4.JMockit;
 import org.apache.commons.compress.compressors.brotli.BrotliCompressorInputStream;
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.meteogroup.jbrotli.Brotli;
-import org.meteogroup.jbrotli.BrotliCompressor;
-import org.meteogroup.jbrotli.BrotliDeCompressor;
-import org.meteogroup.jbrotli.io.BrotliInputStream;
 import org.meteogroup.jbrotli.io.BrotliOutputStream;
 import org.meteogroup.jbrotli.libloader.BrotliLibraryLoader;
 
@@ -16,6 +12,8 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Cosmin Baciu
@@ -23,6 +21,82 @@ import java.nio.file.Paths;
  */
 @RunWith(JMockit.class)
 public class CompressTest {
+
+    @Test
+    public void splitFile() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("c:/DEV/_work/compression/compressed", "r");
+//        RandomAccessFile raf = new RandomAccessFile("c:/DEV/_work/compression/test-e38f79d9-e3c9-4639-a08a-b8f782c99d44", "r");
+        long numSplits = 3; //from user input, extract it from args
+        long sourceSize = raf.length();
+        long bytesPerSplit = sourceSize / numSplits;
+        long remainingBytes = sourceSize % numSplits;
+
+        int maxReadBufferSize = 8 * 1024; //8KB
+        for (int destIx = 1; destIx <= numSplits; destIx++) {
+            BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream("c:/DEV/_work/compression/split." + destIx));
+            if (bytesPerSplit > maxReadBufferSize) {
+                long numReads = bytesPerSplit / maxReadBufferSize;
+                long numRemainingRead = bytesPerSplit % maxReadBufferSize;
+                for (int i = 0; i < numReads; i++) {
+                    readWrite(raf, bw, maxReadBufferSize);
+                }
+                if (numRemainingRead > 0) {
+                    readWrite(raf, bw, numRemainingRead);
+                }
+            } else {
+                readWrite(raf, bw, bytesPerSplit);
+            }
+            bw.close();
+        }
+        if (remainingBytes > 0) {
+            BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream("c:/DEV/_work/compression/split." + (numSplits + 1)));
+            readWrite(raf, bw, remainingBytes);
+            bw.close();
+        }
+        raf.close();
+    }
+
+    public static void mergeFiles(List<File> files, OutputStream mergingStream)
+            throws IOException {
+
+            for (File f : files) {
+                Files.copy(f.toPath(), mergingStream);
+            }
+
+    }
+
+    @Test
+    public void joinFiles() throws IOException {
+        File output = new File("c:/DEV/_work/compression/joined");
+
+        try (OutputStream bw = new BufferedOutputStream(Files.newOutputStream(output.toPath()))) {
+            findAndAppend(new File("c:/DEV/_work/compression"), bw);
+        } catch (IOException exp) {
+            exp.printStackTrace();
+        }
+    }
+
+    public static void findAndAppend(File parent, OutputStream bw) throws IOException {
+
+        // Find any matching files...
+        File files[] = parent.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().contains("split");
+            }
+        });
+
+        mergeFiles(Arrays.asList(files), bw);
+    }
+
+
+    protected void readWrite(RandomAccessFile raf, BufferedOutputStream bw, long numBytes) throws IOException {
+        byte[] buf = new byte[(int) numBytes];
+        int val = raf.read(buf);
+        if (val != -1) {
+            bw.write(buf);
+        }
+    }
 
     //working
     @Test
@@ -55,7 +129,8 @@ public class CompressTest {
 
     @Test
     public void testCommonDecompress() throws IOException {
-        InputStream fin = Files.newInputStream(Paths.get("c:/DEV/_work/compression/compressed"));
+//        InputStream fin = Files.newInputStream(Paths.get("c:/DEV/_work/compression/compressed"));
+        InputStream fin = Files.newInputStream(Paths.get("c:/DEV/_work/compression/joined"));
         BufferedInputStream in = new BufferedInputStream(fin);
         OutputStream out = Files.newOutputStream(Paths.get("c:/DEV/_work/compression/decompressed"));
         BrotliCompressorInputStream brIn = new BrotliCompressorInputStream(in);
@@ -68,36 +143,37 @@ public class CompressTest {
         brIn.close();
     }
 
-    @Test
-    public void testCompression() throws IOException {
-        BrotliLibraryLoader.loadBrotli();
+//    @Test
+//    public void testCompression() throws IOException {
+//        BrotliLibraryLoader.loadBrotli();
+//
+//        final Path toCompress = Paths.get("c:/DEV/_work/compression/test-e38f79d9-e3c9-4639-a08a-b8f782c99d44");
+////        final Path toCompress = Paths.get("c:/DEV/_work/test-da97014e-6515-4bcf-bd07-35df52431edb");
+//        final InputStream inputStream = new BufferedInputStream(Files.newInputStream(toCompress));
+//
+//        final Path compressed = Paths.get("c:/DEV/_work/compression/compressed");
+//        final OutputStream outputStream = Files.newOutputStream(compressed);
+//        Brotli.Parameter parameter = Brotli.DEFAULT_PARAMETER.setQuality(4);
+//        BrotliOutputStream brotliOutputStream = new BrotliOutputStream(outputStream, parameter);
+//
+//        byte[] chunk = new byte[1024];
+//        int chunkLen = 0;
+//        int offset = 0;
+//        while ((chunkLen = inputStream.read(chunk)) != -1) {
+////            brotliOutputStream.write(chunk, offset, chunkLen);
+//            brotliOutputStream.write(chunk);
+//            chunk = new byte[1024];
+//        }
+//
+//      /*  BrotliStreamCompressor brotliStreamCompressor = new BrotliStreamCompressor(parameter);
+//        final byte[] bytes = brotliStreamCompressor.compressArray(new byte[0], 0, 0, true);
+//        outputStream.write(bytes);*/
+//
+////        brotliOutputStream.finish();
+//        brotliOutputStream.close();
+//    }
 
-        final Path toCompress = Paths.get("c:/DEV/_work/compression/test-e38f79d9-e3c9-4639-a08a-b8f782c99d44");
-//        final Path toCompress = Paths.get("c:/DEV/_work/test-da97014e-6515-4bcf-bd07-35df52431edb");
-        final InputStream inputStream = new BufferedInputStream(Files.newInputStream(toCompress));
-
-        final Path compressed = Paths.get("c:/DEV/_work/compression/compressed");
-        final OutputStream outputStream = Files.newOutputStream(compressed);
-        Brotli.Parameter parameter = Brotli.DEFAULT_PARAMETER.setQuality(4);
-        BrotliOutputStream brotliOutputStream = new BrotliOutputStream(outputStream, parameter);
-
-        byte[] chunk = new byte[1024];
-        int chunkLen = 0;
-        int offset = 0;
-        while ((chunkLen = inputStream.read(chunk)) != -1) {
-//            brotliOutputStream.write(chunk, offset, chunkLen);
-            brotliOutputStream.write(chunk);
-            chunk = new byte[1024];
-        }
-
-      /*  BrotliStreamCompressor brotliStreamCompressor = new BrotliStreamCompressor(parameter);
-        final byte[] bytes = brotliStreamCompressor.compressArray(new byte[0], 0, 0, true);
-        outputStream.write(bytes);*/
-
-//        brotliOutputStream.finish();
-        brotliOutputStream.close();
-    }
-
+/*
     @Test
     public void testCompression1() throws IOException {
         BrotliLibraryLoader.loadBrotli();
@@ -154,6 +230,6 @@ public class CompressTest {
         final byte[] out = new byte[in.length];
         brotliDeCompressor.deCompress(in, out);
     }
-
+*/
 
 }
