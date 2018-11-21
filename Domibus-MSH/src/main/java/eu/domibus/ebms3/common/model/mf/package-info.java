4a7a9381a0c540093ecd748a700f2f0075e08f17
baciuco <cosmin.baciu@ext.ec.europa.eu>
2018-11-21 09:58:29 +0100

    [EDELIVERY-4086] - Added JAXB classes for the message fragment

---
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/common/model/mf/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2003/05/soap-envelope", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package eu.domibus.ebms3.common.model.mf;
