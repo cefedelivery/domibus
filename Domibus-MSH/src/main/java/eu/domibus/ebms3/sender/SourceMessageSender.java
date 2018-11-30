ee072b3723ccababde0b47113db7e6ccaddf00a6
baciuco <cosmin.baciu@ext.ec.europa.eu>
2018-11-30 15:46:46 +0100

    [EDELIVERY-4087] - Start implementing SourceMessageSender

--- Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/SourceMessageSender.java
+++ Domibus-MSH/src/main/java/eu/domibus/ebms3/sender/SourceMessageSender.java
@@ -1,9 +1,33 @@
 package eu.domibus.ebms3.sender;
 
+import eu.domibus.api.message.attempt.MessageAttempt;
+import eu.domibus.api.message.attempt.MessageAttemptService;
+import eu.domibus.api.message.attempt.MessageAttemptStatus;
+import eu.domibus.api.security.ChainCertificateInvalidException;
+import eu.domibus.common.ErrorCode;
+import eu.domibus.common.MSHRole;
+import eu.domibus.common.dao.UserMessageLogDao;
+import eu.domibus.common.exception.ConfigurationException;
+import eu.domibus.common.exception.EbMS3Exception;
+import eu.domibus.common.model.configuration.LegConfiguration;
+import eu.domibus.common.model.configuration.Party;
+import eu.domibus.common.services.MessageExchangeService;
+import eu.domibus.common.services.ReliabilityService;
+import eu.domibus.core.pmode.PModeProvider;
 import eu.domibus.ebms3.common.model.UserMessage;
 import eu.domibus.logging.DomibusLogger;
 import eu.domibus.logging.DomibusLoggerFactory;
+import eu.domibus.logging.DomibusMessageCode;
+import eu.domibus.pki.PolicyService;
+import org.apache.commons.lang3.Validate;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.neethi.Policy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.soap.SOAPFaultException;
+import java.sql.Timestamp;
 
 /**
  * @author Cosmin Baciu
@@ -14,8 +38,127 @@ public class SourceMessageSender implements MessageSender {
 
     private static final DomibusLogger LOG = DomibusLoggerFactory.getLogger(SourceMessageSender.class);
 
+    @Autowired
+    private PModeProvider pModeProvider;
+
+    @Autowired
+    private MSHDispatcher mshDispatcher;
+
+    @Autowired
+    private EbMS3MessageBuilder messageBuilder;
+
+    @Autowired
+    private ReliabilityChecker reliabilityChecker;
+
+    @Autowired
+    private ResponseHandler responseHandler;
+
+    @Autowired
+    private MessageAttemptService messageAttemptService;
+
+    @Autowired
+    private MessageExchangeService messageExchangeService;
+
+    @Autowired
+    PolicyService policyService;
+
+    @Autowired
+    private ReliabilityService reliabilityService;
+
+    @Autowired
+    UserMessageLogDao userMessageLogDao;
+
     @Override
     public void sendMessage(final UserMessage userMessage) {
-        LOG.info("-------------SourceMessageSender");
+        String messageId = userMessage.getMessageInfo().getMessageId();
+
+        MessageAttempt attempt = new MessageAttempt();
+        attempt.setMessageId(messageId);
+        attempt.setStartDate(new Timestamp(System.currentTimeMillis()));
+        MessageAttemptStatus attemptStatus = MessageAttemptStatus.SUCCESS;
+        String attemptError = null;
+
+
+        ReliabilityChecker.CheckResult reliabilityCheckSuccessful = ReliabilityChecker.CheckResult.SEND_FAIL;
+        // Assuming that everything goes fine
+        ResponseHandler.CheckResult isOk = ResponseHandler.CheckResult.OK;
+
+        LegConfiguration legConfiguration = null;
+        final String pModeKey;
+
+
+        try {
+            pModeKey = pModeProvider.findUserMessageExchangeContext(userMessage, MSHRole.SENDING).getPmodeKey();
+            LOG.debug("PMode key found : " + pModeKey);
+            legConfiguration = pModeProvider.getLegConfiguration(pModeKey);
+            LOG.info("Found leg [{}] for PMode key [{}]", legConfiguration.getName(), pModeKey);
+
+            Policy policy;
+            try {
+                policy = policyService.parsePolicy("policies/" + legConfiguration.getSecurity().getPolicy());
+            } catch (final ConfigurationException e) {
+                EbMS3Exception ex = new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0010, "Policy configuration invalid", null, e);
+                ex.setMshRole(MSHRole.SENDING);
+                throw ex;
+            }
+
+            Party sendingParty = pModeProvider.getSenderParty(pModeKey);
+            Validate.notNull(sendingParty, "Initiator party was not found");
+            Party receiverParty = pModeProvider.getReceiverParty(pModeKey);
+            Validate.notNull(receiverParty, "Responder party was not found");
+
+            try {
+                messageExchangeService.verifyReceiverCertificate(legConfiguration, receiverParty.getName());
+                messageExchangeService.verifySenderCertificate(legConfiguration, sendingParty.getName());
+            } catch (ChainCertificateInvalidException cciEx) {
+                LOG.securityError(DomibusMessageCode.SEC_INVALID_X509CERTIFICATE, cciEx, null);
+                attemptError = cciEx.getMessage();
+                attemptStatus = MessageAttemptStatus.ABORT;
+                // this flag is used in the finally clause
+                reliabilityCheckSuccessful = ReliabilityChecker.CheckResult.ABORT;
+                LOG.error("Cannot handle request for message:[{}], Certificate is not valid or it has been revoked ", messageId, cciEx);
+                LOG.info("Skipped checking the reliability for message [" + messageId + "]: message sending has been aborted");
+                return;
+            }
+
+            LOG.debug("PMode found : " + pModeKey);
+            final SOAPMessage soapMessage = messageBuilder.buildSOAPMessage(userMessage, legConfiguration);
+            final SOAPMessage response = mshDispatcher.dispatch(soapMessage, receiverParty.getEndpoint(), policy, legConfiguration, pModeKey);
+            isOk = responseHandler.handle(response);
+            if (ResponseHandler.CheckResult.UNMARSHALL_ERROR.equals(isOk)) {
+                EbMS3Exception e = new EbMS3Exception(ErrorCode.EbMS3ErrorCode.EBMS_0004, "Problem occurred during marshalling", messageId, null);
+                e.setMshRole(MSHRole.SENDING);
+                throw e;
+            }
+            reliabilityCheckSuccessful = reliabilityChecker.check(soapMessage, response, pModeKey);
+        } catch (final SOAPFaultException soapFEx) {
+            if (soapFEx.getCause() instanceof Fault && soapFEx.getCause().getCause() instanceof EbMS3Exception) {
+                reliabilityChecker.handleEbms3Exception((EbMS3Exception) soapFEx.getCause().getCause(), messageId);
+            } else {
+                LOG.warn("Error for message with ID [" + messageId + "]", soapFEx);
+            }
+            attemptError = soapFEx.getMessage();
+            attemptStatus = MessageAttemptStatus.ERROR;
+        } catch (final EbMS3Exception e) {
+            reliabilityChecker.handleEbms3Exception(e, messageId);
+            attemptError = e.getMessage();
+            attemptStatus = MessageAttemptStatus.ERROR;
+        } catch (Throwable t) {
+            //NOSONAR: Catching Throwable is done on purpose in order to even catch out of memory exceptions in case large files are sent.
+            LOG.error("Error sending message [{}]", messageId, t);
+            attemptError = t.getMessage();
+            attemptStatus = MessageAttemptStatus.ERROR;
+            throw t;
+        } finally {
+            try {
+                reliabilityService.handleReliability(messageId, userMessage, reliabilityCheckSuccessful, isOk, legConfiguration);
+                attempt.setError(attemptError);
+                attempt.setStatus(attemptStatus);
+                attempt.setEndDate(new Timestamp(System.currentTimeMillis()));
+                messageAttemptService.create(attempt);
+            } catch (Exception ex) {
+                LOG.error("Finally: ", ex);
+            }
+        }
     }
 }
