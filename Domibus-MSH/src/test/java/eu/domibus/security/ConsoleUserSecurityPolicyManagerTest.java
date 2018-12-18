588bd1db25fd704b93cf1212da713a6caa3e3e04
ion.perpegel <ion.perpegel@gmail.com>
2018-12-18 13:30:58 +0200

    [EDELIVERY-4210] add new test class

---
+++ Domibus-MSH/src/test/java/eu/domibus/security/ConsoleUserSecurityPolicyManagerTest.java
@@ -0,0 +1,80 @@
+package eu.domibus.security;
+
+import eu.domibus.api.multitenancy.DomainContextProvider;
+import eu.domibus.api.multitenancy.DomainService;
+import eu.domibus.api.multitenancy.UserDomainService;
+import eu.domibus.api.property.DomibusPropertyProvider;
+import eu.domibus.common.dao.security.ConsoleUserPasswordHistoryDao;
+import eu.domibus.common.dao.security.UserDao;
+import eu.domibus.core.alerts.service.ConsoleUserAlertsServiceImpl;
+import eu.domibus.core.alerts.service.PluginUserAlertsServiceImpl;
+import eu.domibus.core.security.AuthenticationDAO;
+import eu.domibus.core.security.PluginUserPasswordHistoryDao;
+import mockit.Injectable;
+import mockit.Tested;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author Ion Perpegel
+ * @since 4.1
+ */
+
+public class ConsoleUserSecurityPolicyManagerTest {
+
+    @Injectable
+    DomibusPropertyProvider domibusPropertyProvider;
+
+    @Injectable
+    ConsoleUserPasswordHistoryDao userPasswordHistoryDao;
+
+    @Injectable
+    UserDao userDao;
+
+    @Injectable
+    BCryptPasswordEncoder bcryptEncoder;
+
+    @Injectable
+    ConsoleUserAlertsServiceImpl userAlertsService;
+
+    @Injectable
+    UserDomainService userDomainService;
+
+    @Injectable
+    DomainService domainService;
+
+    @Injectable
+    DomainContextProvider domainContextProvider;
+
+    @Tested
+    ConsoleUserSecurityPolicyManager passwordManager;
+
+    @Test
+    public void testGetPasswordComplexityPatternProperty() {
+        String result = passwordManager.getPasswordComplexityPatternProperty();
+        Assert.assertEquals(ConsoleUserSecurityPolicyManager.PASSWORD_COMPLEXITY_PATTERN, result);
+    }
+
+    @Test
+    public void testGetPasswordHistoryPolicyProperty() {
+        String result = passwordManager.getPasswordHistoryPolicyProperty();
+        Assert.assertEquals(ConsoleUserSecurityPolicyManager.PASSWORD_HISTORY_POLICY, result);
+    }
+
+    @Test
+    public void testGetMaximumDefaultPasswordAgeProperty() {
+        String result = passwordManager.getMaximumDefaultPasswordAgeProperty();
+        Assert.assertEquals(ConsoleUserSecurityPolicyManager.MAXIMUM_DEFAULT_PASSWORD_AGE, result);
+    }
+    @Test
+    public void testGetMaximumPasswordAgeProperty() {
+        String result = passwordManager.getMaximumPasswordAgeProperty();
+        Assert.assertEquals(ConsoleUserSecurityPolicyManager.MAXIMUM_PASSWORD_AGE, result);
+    }
+    @Test
+    public void testGetWarningDaysBeforeExpiration() {
+        String result = passwordManager.getWarningDaysBeforeExpiration();
+        Assert.assertEquals(ConsoleUserSecurityPolicyManager.WARNING_DAYS_BEFORE_EXPIRATION, result);
+    }
+}
