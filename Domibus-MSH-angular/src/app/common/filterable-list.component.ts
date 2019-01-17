f6b3ff24388a4605aff09f4b67b45d46b327f647
ion.perpegel <ion.perpegel@gmail.com>
2019-01-17 11:37:50 +0200

    [EDELIVERY-3738] implement pull request comments

--- Domibus-MSH-angular/src/app/common/filterable-list.component.ts
+++ Domibus-MSH-angular/src/app/common/filterable-list.component.ts
@@ -1,5 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
+/**
+ * Base class for components that display a list of items that can be filtered
+ * It is an embrion; more common functionality will be added in time
+ *
+ * @since 4.1
+ */
+
 @Component({
   moduleId: module.id,
   selector: 'filtered-list',
@@ -17,6 +24,10 @@ export class FilterableListComponent implements OnInit {
     this.filter = {};
   }
 
+  /**
+   * The method takes the filter params set through widgets and copies them to the active params
+   * active params are the ones that are used for actual filtering of data and can be different from the ones set by the user in the UI
+   */
   protected setActiveFilter() {
     if (!this.activeFilter) {
       this.activeFilter = {};
@@ -24,6 +35,9 @@ export class FilterableListComponent implements OnInit {
     Object.assign(this.activeFilter, this.filter);
   }
 
+  /**
+   * The method takes the actual filter params and copies them to the UI bound params thus synchronizing the pair so what you see it is what you get
+   */
   protected resetFilters() {
     this.filter = {};
     Object.assign(this.filter, this.activeFilter);
