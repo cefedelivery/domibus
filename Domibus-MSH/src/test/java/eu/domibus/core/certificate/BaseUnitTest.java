9de7b36ca3feccd603df71ae8c4ee161ca77b124
ion.perpegel <ion.perpegel@gmail.com>
2019-01-23 17:47:38 +0200

    [EDELIVERY-3759] remove unused public method and move it to tests

---
+++ Domibus-MSH/src/test/java/eu/domibus/core/certificate/BaseUnitTest.java
@@ -0,0 +1,29 @@
+package eu.domibus.common.services.impl;
+
+import eu.domibus.pki.DomibusCertificateException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+public class BaseUnitTest {
+
+    protected X509Certificate loadCertificateFromJKSFile(String filePath, String alias, String password) {
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(fileInputStream, password.toCharArray());
+
+            Certificate cert = keyStore.getCertificate(alias);
+
+            return (X509Certificate) cert;
+        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException e) {
+            throw new DomibusCertificateException("Could not load certificate from file " + filePath + ", alias " + alias, e);
+        }
+    }
+}
