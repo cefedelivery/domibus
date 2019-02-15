4c16060645b170b8ab9c75501ebdedaaa37e9e03
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2019-02-15 15:00:30 +0100

    EDELIVERY-4378 Default properties not overwrite correctly
    Fix line length issue

--- Domibus-MSH/src/main/java/eu/domibus/configuration/DomibusPropertyConfig.java
+++ Domibus-MSH/src/main/java/eu/domibus/configuration/DomibusPropertyConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 /**
- * <p>Configuration providing common property sources to the {@code Environment} in order to ensure the placeholders get correctly replaced inside bean definition property values
- * and {@code @Value} fields. It is intended to be used as a replacement for individual {@code PropertySource} annotations placed on classes implementing {@code Condition}.</p>
+ * <p>Configuration providing common property sources to the {@code Environment} in order
+ * to ensure the placeholders get correctly replaced inside bean definition property values
+ * and {@code @Value} fields. It is intended to be used as a replacement for individual
+ * {@code PropertySource} annotations placed on classes implementing {@code Condition}.</p>
  *
  * @author Sebastian-Ion TINCU
  * @see org.springframework.core.env.Environment
@@ -14,7 +16,8 @@ import org.springframework.context.annotation.PropertySources;
  * @since 4.1
  */
 @Configuration
-// The following property sources are being checked in the reverse order (first the file://../domibus.properties, then the classpath://../domibus.properties
+// The following property sources are being checked in the reverse order (first
+// the file://../domibus.properties, then the classpath://../domibus.properties
 // and finally the classpath://../domibus-default.properties).
 @PropertySources({
         @PropertySource("classpath:config/domibus-default.properties"),
