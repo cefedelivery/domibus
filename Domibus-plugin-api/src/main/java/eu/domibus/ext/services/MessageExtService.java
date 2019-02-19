1679bbbd9ac8bfeeb9281ca05718b75757899ea0
TINCU Sebastian-Ion <Sebastian-Ion.TINCU@ext.ec.europa.eu>
2019-02-19 16:26:38 +0100

    Remove redundant #stripToEmpty(String) as per PR comments.

--- Domibus-plugin-api/src/main/java/eu/domibus/ext/services/MessageExtService.java
+++ Domibus-plugin-api/src/main/java/eu/domibus/ext/services/MessageExtService.java
@@ -10,10 +10,10 @@ import eu.domibus.ext.exceptions.MessageExtException;
 public interface MessageExtService {
 
     /**
-     * Returns a clean message identifier free from any whitespace and control character present at its beginning or its end.
+     * Returns a clean message identifier free from any control character present at its beginning or its end.
      *
      * @param messageId the message identifier to clean up
-     * @return The message identifier with all the whitespace and control characters removed from its beginning and its end
+     * @return The message identifier with all the control characters removed from its beginning and its end
      * or the empty string {@code ""} if the message identifier is only made up of these characters.
      * @throws MessageExtException when something wrong occurs while trying to clean the message identifier
      */
