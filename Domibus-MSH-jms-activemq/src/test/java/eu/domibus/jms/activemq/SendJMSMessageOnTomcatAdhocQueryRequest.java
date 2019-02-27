4115eeb131f811b1fea45bd2f180ccf4b0149549
baciuco <cosmin.baciu@ext.ec.europa.eu>
2019-02-27 08:40:05 +0100

    [EDELIVERY-4260] - Test resources added

--- Domibus-MSH-jms-activemq/src/test/java/eu/domibus/jms/activemq/SendJMSMessageOnTomcatAdhocQueryRequest.java
+++ Domibus-MSH-jms-activemq/src/test/java/eu/domibus/jms/activemq/SendJMSMessageOnTomcatAdhocQueryRequest.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.UUID;
 
-public class SendJMSMessageOnTomcat {
+public class SendJMSMessageOnTomcatAdhocQueryRequest {
 
     public static void main(String[] args) {
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61617");
@@ -42,26 +42,24 @@ public class SendJMSMessageOnTomcat {
             // messageMap.setStringProperty("refToMessageId", "0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-ae1a-4c1b-ad62-2b18ee57e39e@domibus.eu0079a47e-0079a47e-aesasa");
 
             // Set up the Communication properties for the message
-            messageMap.setStringProperty("service", "bdx:noprocess");
-            //messageMap.setStringProperty("serviceType", "noSecurity");
-            //messageMap.setStringProperty("serviceType", "signOnly");
-            messageMap.setStringProperty("serviceType", "tc1");
+            messageMap.setStringProperty("service", "urn:ihe:iti:2007:CrossGatewayQuery");
+            messageMap.setStringProperty("serviceType", "eHSDI");
 
-            messageMap.setStringProperty("action", "TC1Leg1");
+            messageMap.setStringProperty("action", "urn:ihe:iti:2007:CrossGatewayQuery");
             messageMap.setStringProperty("conversationId", "123");
             //messageMap.setStringProperty("fromPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:domibus-blue");
             //messageMap.setStringProperty("fromPartyType", ""); // Mandatory but empty here because it is in the value of the party ID
             messageMap.setStringProperty("fromPartyId", "domibus-blue");
-            messageMap.setStringProperty("fromPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered"); // Mandatory
+            messageMap.setStringProperty("fromPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:eHSDI"); // Mandatory
 
-            messageMap.setStringProperty("fromRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator");
+            messageMap.setStringProperty("fromRole", "urn:ihe:iti:2018:Requester");
 
             //messageMap.setStringProperty("toPartyId", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:domibus-red");
             //messageMap.setStringProperty("toPartyType", ""); // Mandatory but empty here because it is in the value of the party ID
             messageMap.setStringProperty("toPartyId", "domibus-red");
-            messageMap.setStringProperty("toPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered"); // Mandatory
+            messageMap.setStringProperty("toPartyType", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:eHSDI"); // Mandatory
 
-            messageMap.setStringProperty("toRole", "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder");
+            messageMap.setStringProperty("toRole", "urn:ihe:iti:2018:Provider");
 
             messageMap.setStringProperty("originalSender", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C1");
             messageMap.setStringProperty("finalRecipient", "urn:oasis:names:tc:ebcore:partyid-type:unregistered:C4");
@@ -71,10 +69,10 @@ public class SendJMSMessageOnTomcat {
             //Set up the payload properties
             messageMap.setStringProperty("totalNumberOfPayloads", "1");
 
-            messageMap.setStringProperty("payload_1_mimeContentId", "cid:message");
+            messageMap.setStringProperty("payload_1_mimeContentId", "");
             messageMap.setStringProperty("payload_1_description", "message");
-            messageMap.setStringProperty("payload_1_mimeType", "text/xml");
-//            messageMap.setStringProperty("p1InBody", "true"); // If true payload_1 will be sent in the body of the AS4 message. Only XML payloads may be sent in the AS4 message body. Optional
+            messageMap.setStringProperty("payload_1_mimeType", "application/xml");
+            messageMap.setStringProperty("p1InBody", "true"); // If true payload_1 will be sent in the body of the AS4 message. Only XML payloads may be sent in the AS4 message body. Optional
 
 
 //            messageMap.setStringProperty("payload_2_description", "message");
@@ -91,7 +89,7 @@ public class SendJMSMessageOnTomcat {
 
 
             //send the payload in the JMS message as byte array
-            String pay1 = "<test>test</test>";
+            String pay1 = IOUtils.toString(new ClassPathResource("AdhocQueryRequest.xml").getInputStream(), utf8Encoding);
             byte[] payload = pay1.getBytes();
             messageMap.setBytes("payload_1", payload);
 //            messageMap.setBytes("payload_2", payload);
