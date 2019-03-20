bd778ba4bef2cc48686d4e4820055a748c0135ef
Catalin ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2019-03-20 10:41:24 +0100

    [EDELIVERY-4435] security spring config optimization

--- Domibus-MSH/src/main/java/eu/domibus/configuration/security/AbstractWebSecurityConfigurerAdapter.java
+++ Domibus-MSH/src/main/java/eu/domibus/configuration/security/AbstractWebSecurityConfigurerAdapter.java
@@ -1,9 +1,18 @@
 package eu.domibus.configuration.security;
 
+import eu.domibus.api.security.AuthRole;
+import eu.domibus.web.filter.SetDomainFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.ws.rs.HttpMethod;
 
 /**
  * Abstract class for Domibus security configuration
@@ -15,14 +24,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @since 4.1
  */
 public abstract class AbstractWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    @Autowired
+    CsrfTokenRepository tokenRepository;
+
+    @Autowired
+    RequestMatcher csrfURLMatcher;
+
+    @Autowired
+    SetDomainFilter setDomainFilter;
+
+    @Autowired
+    Http403ForbiddenEntryPoint http403ForbiddenEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        configureHttpSecurityCommon(http);
         configureHttpSecurity(http);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        configureWebSecurityCommon(web);
         configureWebSecurity(web);
     }
 
@@ -38,19 +60,74 @@ public abstract class AbstractWebSecurityConfigurerAdapter extends WebSecurityCo
      * @param http
      * @throws Exception
      */
-    public abstract void configureHttpSecurity(HttpSecurity http) throws Exception;
+    protected abstract void configureHttpSecurity(HttpSecurity http) throws Exception;
 
     /**
-     * to be implemented
-     * @param auth
+     * configure {@link WebSecurity} - to be implemented
+     *
+     * @param web
+     * @throws Exception
+     */
+    protected abstract void configureWebSecurity(WebSecurity web) throws Exception;
+
+    /**
+     * configure {@link AuthenticationManagerBuilder} to be implemented
+     * @param auth an {@link AuthenticationManagerBuilder}
      */
     protected abstract void configureAuthenticationManagerBuilder(AuthenticationManagerBuilder auth) throws  Exception;
 
 
-    protected void configureWebSecurity(WebSecurity web) {
+    /**
+     * common web security common configuration
+     * @param web {@link WebSecurity} to configure
+     */
+    private void configureWebSecurityCommon(WebSecurity web) {
         web
                 .ignoring().antMatchers("/services/**")
                 .and()
                 .ignoring().antMatchers("/ext/**");
+    }
+
+    /**
+     * common http security config
+     *
+     * @param httpSecurity
+     */
+    private void configureHttpSecurityCommon(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().csrfTokenRepository(tokenRepository).requireCsrfProtectionMatcher(csrfURLMatcher)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/index.html", "/login",
+                        "/rest/security/authentication",
+                        "/rest/application/info",
+                        "/rest/application/name",
+                        "/rest/application/fourcornerenabled",
+                        "/rest/application/extauthproviderenabled",
+                        "/rest/application/multitenancy",
+                        "/rest/application/supportteam",
+                        "/rest/security/username").permitAll()
+                .antMatchers("/rest/application/domains").hasAnyAuthority(AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/rest/security/user/password").authenticated()
+                .antMatchers(HttpMethod.PUT, "/rest/security/user/domain").hasAnyAuthority(AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/pmode/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/party/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/truststore/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/messagefilters/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/jms/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/user/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/plugin/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/audit/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/alerts/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/testservice/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/logging/**").hasAnyAuthority(AuthRole.ROLE_ADMIN.name(), AuthRole.ROLE_AP_ADMIN.name())
+                .antMatchers("/rest/**").authenticated()
+                .and()
+                .exceptionHandling().and()
+                .headers().frameOptions().deny().contentTypeOptions().and().xssProtection().xssProtectionEnabled(true).and()
+                .and()
+                .httpBasic().authenticationEntryPoint(http403ForbiddenEntryPoint)
+                .and()
+                .addFilterBefore(setDomainFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
