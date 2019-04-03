964f56899dd6de8dec220a345238397f7b7df143
dussath <thomas.dussart@ext.ce.europa.eu>
2019-04-03 17:49:13 +0200

    EDELIVERY-4328: add comments

--- Domibus-MSH/src/main/java/eu/domibus/common/metrics/Timer.java
+++ Domibus-MSH/src/main/java/eu/domibus/common/metrics/Timer.java
@@ -7,13 +7,22 @@ import java.lang.annotation.Target;
 
 /**
  * @author Thomas Dussart
- * @since 4.0
+ * @since 4.1
+ *
+ * Metric annotation to add a timer. The current implementation is using dropwizard
+ * timer.
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Timer {
+    /**
+     * @return the timer name.
+     */
     String value();
 
+    /**
+     * @return the timer class.
+     */
     Class<?> clazz() default Default.class;
 }
 
